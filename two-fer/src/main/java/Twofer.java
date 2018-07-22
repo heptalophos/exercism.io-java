6274fbd7f8158b69804a8e571f768ec70e651b0b
Zero Delta <zerodeltahedge@gmail.com>
2018-07-22 17:25:33 +0300

    two-fer

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,5 @@
 class Twofer {
     String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return "One for " + (name != null ? name : "you") + ", one for me.";
     }
 }
