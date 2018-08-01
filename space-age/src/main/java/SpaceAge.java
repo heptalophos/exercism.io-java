ed3cd378fc670b2849fbdea11e2073edf182902d
Zero Delta <zerodeltahedge@gmail.com>
2018-08-01 13:21:59 +0300

    space-age all tests passing

--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -1,43 +1,49 @@
-class SpaceAge {
+public class SpaceAge {
 
-    SpaceAge(double seconds) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private Long seconds;
+
+    public SpaceAge(Long seconds) {
+        this.seconds = seconds;
+    }
+
+    public SpaceAge(Integer seconds) {
+        this.seconds = new Long(seconds);
     }
 
     double getSeconds() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds;
     }
 
     double onEarth() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / 31557600.0;
     }
 
     double onMercury() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 0.2408467;
     }
 
     double onVenus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 0.61519726;
     }
 
     double onMars() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 1.8808158;
     }
 
     double onJupiter() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 11.862615;
     }
 
     double onSaturn() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 29.447498;
     }
 
     double onUranus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 84.016846;
     }
 
     double onNeptune() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return onEarth() / 164.79132;
     }
 
 }
