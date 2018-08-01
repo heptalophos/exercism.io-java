773626175500093c18fd07350a83cd3f9746a0a9
Zero Delta <zerodeltahedge@gmail.com>
2018-08-01 18:39:27 +0300

    hamming

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,30 @@
-class Hamming {
+// import com.google.guava.Stream;
+import com.codepoetics.protonpack.StreamUtils;
+import java.util.stream.Stream;
 
-    Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class Hamming {
+
+    private final int hammingDistance;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        hammingDistance = computeHamming(leftStrand, rightStrand);
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return hammingDistance;
+    }
+
+    private int computeHamming(String leftStrand, String rightStrand) {
+
+        if (leftStrand.length() != rightStrand.length())
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+
+        Stream<Character> leftStream = leftStrand.chars().mapToObj(i -> (char) i);
+        Stream<Character> rightStream = rightStrand.chars().mapToObj(i -> (char) i);
+
+        return StreamUtils
+               .zip(leftStream, rightStream, (left, right) -> left.equals(right) ? 0 : 1)
+               .reduce(0, Integer::sum);
     }
 
 }
