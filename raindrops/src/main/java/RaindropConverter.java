1f9fb90d7dd7a56d7228dd63c30b65bc91c4b40a
Zero Delta <zerodeltahedge@gmail.com>
2018-08-21 17:05:27 +0300

    raindrops passing all tests

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,12 @@
-class RaindropConverter {
+public class RaindropConverter {
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public static String convert(int number) {
+        StringBuilder drops = new StringBuilder() ;
+        drops.append(number % 3 == 0 ? "Pling" : "");
+        drops.append(number % 5 == 0 ? "Plang" : "");
+        drops.append(number % 7 == 0 ? "Plong" : "");
+        drops = drops.length() == 0 ? drops.append(number) : drops;
+        return drops.toString();
     }
 
 }
