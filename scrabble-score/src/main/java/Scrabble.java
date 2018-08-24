4ab5f944c390512bcbc96710ddbdd2cd49f3bfca
Zero Delta <zerodeltahedge@gmail.com>
2018-08-24 18:48:38 +0300

    scrabble-score 1st ireration

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,32 @@
-class Scrabble {
+import java.util.HashMap;
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class Scrabble {
+
+    private final String word;
+    private static final HashMap<Integer, Integer> letterScore = new HashMap<>('Z' - 'A');
+
+    static {
+        "AEIOULNRST".chars().forEach(letter -> letterScore.put(letter, 1));
+        "DG".chars().forEach(letter -> letterScore.put(letter, 2));
+        "BCMP".chars().forEach(letter -> letterScore.put(letter, 3));
+        "FHVWY".chars().forEach(letter -> letterScore.put(letter, 4));
+        "K".chars().forEach(letter -> letterScore.put(letter, 5));
+        "JX".chars().forEach(letter -> letterScore.put(letter, 8));
+        "QZ".chars().forEach(letter -> letterScore.put(letter, 10));
     }
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+    public Scrabble(String word) {
+        this.word = word;
+    }
+
+    public int getScore() {
+        if (word == null) return 0;
+        return word
+               .toUpperCase()
+               .chars()
+               .map(ch -> letterScore.getOrDefault(ch, 0))
+               .reduce(0, (a, b) -> a + b);
     }
 
 }
