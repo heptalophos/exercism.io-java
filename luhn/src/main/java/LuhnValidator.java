f366bbed5765a99953308b44b68a0627e62bcdb2
Zero Delta <zerodeltahedge@gmail.com>
2018-09-01 02:41:35 +0300

    refactored and re-tested luhn

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,31 +1,21 @@
-// import java.util.Arrays;
-import java.util.stream.IntStream;
-
-
-class LuhnValidator {
-
-    // private String[] digits;
+public class LuhnValidator {
 
     boolean isValid(String candidate) {
-        candidate = candidate.replace(" ", "");
-        if (candidate == null || candidate.length() <= 1 || !candidate.matches("[0-9]")) 
-            return false;
-        long zeros = candidate.chars().filter(ch -> ch == '0').count();  
-        String[] digits = new StringBuilder(candidate).toString().split("");
-        Integer sumLuhn = IntStream.rangeClosed(1, digits.length)
-                                    .map (i -> { int index =  digits.length - i;
-                                                 int digit = Integer.parseInt(digits[index]);
-                                                 return index % 2 == 0 ?
-                                                        2 * digit > 9 ? (2 * digit) - 9 : 2 * digit : 
-                                                        digit;})
-                                    .sum();
-        return ((sumLuhn == 0 && zeros > 1) || (sumLuhn != 0 && sumLuhn % 10 == 0));
+        int length = 0, sumLuhn = 0;
+        boolean flip = false;
+        for (int i = candidate.length() - 1; i >= 0; i--) {
+            char current = candidate.charAt(i);
+            if (current == ' ') continue;
+            if (current < '0' || current > '9') return false;
+            int digit = Integer.parseInt(Character.toString(current));
+            if (flip) {
+                digit *= 2;
+                if (digit > 9) digit -= 9;
+            }
+            sumLuhn += digit;
+            length++;
+            flip = !flip;
+        }
+        return (length > 1) && (sumLuhn % 10 == 0);
     }
-
-    // public static void main(String [] args) {
-    //     LuhnValidator luhnValidator = new LuhnValidator();
-    //     System.out.println("0000 0");
-    //     System.out.println(luhnValidator.isValid("0000 0"));
-    // }
 }
-
