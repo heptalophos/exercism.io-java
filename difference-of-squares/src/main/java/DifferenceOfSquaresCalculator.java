b82066f5caa1be16e398235a6052684c851f5a91
Zero Delta <zerodeltahedge@gmail.com>
2018-09-04 17:10:59 +0300

    refactored difference-of-squares

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -3,15 +3,14 @@ import java.util.stream.IntStream;
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        int sum = IntStream.rangeClosed(1, input)
-                           .reduce(0, (x, y) -> x + y);
-        return sum * sum;
+        return (int) Math.pow(IntStream.rangeClosed(1, input)
+                    .reduce(0, (x, y) -> x + y), 2);
     }
 
     int computeSumOfSquaresTo(int input) {
         return IntStream.rangeClosed(1, input)
-                           .map( i -> i * i)
-                           .reduce(0, (x, y) -> x + y);
+              .map( i -> i * i)
+              .reduce(0, (x, y) -> x + y);
     }
 
     int computeDifferenceOfSquares(int input) {
