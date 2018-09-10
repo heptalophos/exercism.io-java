9028a9806fe3181cf11f787c75bb182ca3b6e372
Zero Delta <zerodeltahedge@gmail.com>
2018-09-10 18:58:53 +0300

    secret-handshake

---
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+class HandshakeCalculator {
+
+    private final List<Signal> actions = new ArrayList<>();
+
+    public List<Signal> calculateHandshake(int number) {
+
+        if ((0x1 & number) > 0) actions.add(Signal.WINK);
+        if ((0x2 & number) > 0) actions.add(Signal.DOUBLE_BLINK) ;
+        if ((0x4 & number) > 0) actions.add(Signal.CLOSE_YOUR_EYES);
+        if ((0x8 & number) > 0) actions.add(Signal.JUMP);
+        if ((0x10 & number) > 0) Collections.reverse(actions);
+        
+        return actions;
+    }
+}
