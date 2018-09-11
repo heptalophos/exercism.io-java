267f9317bfaaa8b08e30f1881646201c3eb129e5
Zero Delta <zerodeltahedge@gmail.com>
2018-09-11 22:31:20 +0300

    isbn-verifier

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -7,7 +7,6 @@ class IsbnVerifier {
         if (normal.length() != 10) { return false; }
         for (int i = 0, j = 10; i < normal.length() - 1; i++, j--) {
             if (Character.isDigit(normal.charAt(i))) {
-                // sum += Integer.valueOf((char)normal.charAt(i)) * (10 - i);
                 sum += Character.getNumericValue(normal.charAt(i)) * j;
             } else { 
                 return false;
