086cd528d64ae1aa0e38f5bb018a7e8e99bb13ca
Zero Delta <zerodeltahedge@gmail.com>
2018-09-18 00:54:42 +0300

    isogram

---
+++ isogram/src/main/java/IsogramChecker.java
@@ -0,0 +1,9 @@
+class IsogramChecker {
+
+    boolean isIsogram(String phrase) {
+        phrase = phrase.replace("-", "").replace(" ", "").toLowerCase();
+        return (phrase.isEmpty())
+               ? true
+               : phrase.chars().distinct().count() == phrase.length();
+    }
+}
