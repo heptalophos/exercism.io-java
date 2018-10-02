9b2b97f6a7fdf5d083fa5bb1122294b78f36b8c5
Zero Delta <zerodeltahedge@gmail.com>
2018-10-02 03:03:28 +0300

    refactor nucleotide-count

--- nucleotide-count/src/main/java/NucleotideCounter.java
+++ nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,25 +1,30 @@
+import java.util.Map;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 
 public class NucleotideCounter {
 
     private final String sequence;
+    private final Map<Character, Integer> tally;
 
     public NucleotideCounter(String seq) {
         sequence = seq;
+        tally = new HashMap<>();
+        "CGAT".chars().forEach(ch -> tally.put((char) ch, 0));
+        this.nucleotideCounts();
     }
 
     public Integer count(Character nuc) {
-        if("CGAT".indexOf(nuc) == -1) {
-            throw new IllegalArgumentException();
-        }
+        if (!"GCAT".contains(Character.toString(nuc))) 
+            throw new IllegalArgumentException(); 
         return (int) sequence.chars().filter(ch -> (char) ch == nuc).count();
     }
 
-    public HashMap<Character, Integer> nucleotideCounts() {
-        final HashMap<Character, Integer> tally = new HashMap<>();
-        "CGAT".chars().forEach(ch -> tally.put((char) ch, 0));
-        sequence.chars().forEach(ch -> tally.put((char) ch, count((char) ch)));
+    public Map<Character, Integer> nucleotideCounts() {
+        for (Character ch : sequence.chars().mapToObj(x -> (char) x).collect(Collectors.toSet())) {
+            tally.put(ch, count(ch));
+        };
         return tally;
     }
 }
