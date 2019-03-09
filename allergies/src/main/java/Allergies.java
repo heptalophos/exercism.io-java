415dc27bbefebeec94b285dac8235f86e91e22ae
Zero Delta <zerodeltahedge@gmail.com>
2019-03-09 06:39:10 +0200

    allergies solution

---
+++ allergies/src/main/java/Allergies.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class Allergies {
+
+    private List<Allergen> allergies;
+
+    public Allergies (int score) {
+        allergies = Stream.of(Allergen.values())
+                          .filter(allergen -> (score & allergen.getScore()) > 0)
+                          .collect(Collectors.toList());
+    }
+
+    public boolean isAllergicTo(Allergen allergen) {
+        return allergies.contains(allergen);
+    }
+
+    public List<Allergen> getList() {
+        return allergies;
+    }
+}
+
