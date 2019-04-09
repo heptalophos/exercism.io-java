09ab0b1ea9505f71439e58ef7f7f76446fc9ee9f
Zero Delta <zerodeltahedge@gmail.com>
2019-04-10 00:09:47 +0300

    alternative sieve solution (java 8)

---
+++ sieve/src/main/java/Sieve2.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+class Sieve {
+
+    private List<Integer> primes;
+
+    public Sieve(int limit) {
+
+        boolean[] notPrime = new boolean[limit + 1];
+
+        // This works for java 8
+        Arrays.fill(notPrime, false);
+
+        for (int p = 2; p * p <=limit; p++) {
+            if (!notPrime[p]) {
+                for (int i = p * p; i <= limit; i += p) {
+                    notPrime[i] = true;
+                }
+            }
+        }
+        
+        primes = IntStream.rangeClosed(2, limit)
+                          .filter(x -> !notPrime[x])
+                          .boxed()
+                          .collect(Collectors.toList());
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+}
