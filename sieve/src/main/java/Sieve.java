c1501694e320018c5772540188f025bd60b2a110
Zero Delta <zerodeltahedge@gmail.com>
2019-04-10 00:09:13 +0300

    refactor sieve solution - Streams based (java9 and above)

--- sieve/src/main/java/Sieve.java
+++ sieve/src/main/java/Sieve.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
@@ -11,23 +10,14 @@ class Sieve {
 
         boolean[] notPrime = new boolean[limit + 1];
 
-        // IntStream.rangeClosed(2, (int) Math.sqrt(lim))
-        //          .filter(x -> !notPrime[x])
-        //          .flatMap(x -> Stream.iterate(x * x, m -> m <= lim, m -> m + x)) 
-        //  // this works in java9 but not in java 8
-        //          .forEach(x -> notPrime[x] = true);
-        // // Also .flatMap(x -> Stream.iterate(x * x, m -> m + x).takeWhile(m -> m <= lim))
-        // // only works in java9 yet, so: 
-
-        Arrays.fill(notPrime, false);
-
-        for (int p = 2; p * p <=limit; p++) {
-            if (!notPrime[p]) {
-                for (int i = p * p; i <= limit; i += p) {
-                    notPrime[i] = true;
-                }
-            }
-        }
+        // this only works with java9 and above
+        IntStream.rangeClosed(2, (int) Math.sqrt(limit))
+                 .filter(x -> !notPrime[x])
+                 .flatMap(x -> IntStream.iterate(x * 2, m -> m + x)
+                                        .takeWhile(m -> m <= limit)) 
+                 // or:
+                 // .flatMap(x -> IntStream.iterate(x * x, m -> m <= limit, m -> m + x))
+                 .forEach(x -> notPrime[x] = true);
         
         primes = IntStream.rangeClosed(2, limit)
                           .filter(x -> !notPrime[x])
