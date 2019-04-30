629a921e26c7f557e905aeb523db637d200eae51
Zero Delta <zerodeltahedge@gmail.com>
2019-04-30 20:17:25 +0300

    refactor wordy

--- wordy/src/main/java/WordProblemSolver.java
+++ wordy/src/main/java/WordProblemSolver.java
@@ -1,9 +1,10 @@
 import java.util.Iterator;
 import java.util.stream.Stream;
+import java.util.NoSuchElementException;
 
 public class WordProblemSolver {
 
-    public char lexOp (String op) {
+    private char lexOp (String op) {
         switch (op) {
             case "plus": return '+';
             case "minus": return '-';
@@ -13,41 +14,47 @@ public class WordProblemSolver {
         }
     }
 
-    private int applyOp (int total, char op, int argument) {
+    private int applyOp (int acc, char op, int argument) {
         switch (op) {
-            case '+': total += argument; break;
-            case '-': total -= argument; break;
-            case '*': total *= argument; break;
-            case '/': total /= argument; break;
+            case '+': acc += argument; break;
+            case '-': acc -= argument; break;
+            case '*': acc *= argument; break;
+            case '/': acc /= argument; break;
             default : throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
         }
-        return total;
+        return acc;
     }
 
     public int solve(String question) {
 
-        Iterator<Integer> numbers = Stream.of(question.split("[^0-9+-]+"))
-                              .filter(x -> x.length() > 0)
-                              .mapToInt(Integer::parseInt)
-                              .iterator();
+        try {
 
-        Iterator<Character> operations = Stream.of(question.replaceAll("What is ", "")
-                                                .replaceAll(" by ", "")
-                                                .split("[^plus|minus|multiplied|divided]"))
-                                  .filter(x -> x.length() > 0)
-                                  .map(x -> lexOp(x))
-                                  .iterator();
+            Iterator<Integer> numbers = Stream.of(question.split("[^0-9+-]+"))
+                                              .filter(x -> x.length() > 0)
+                                              .mapToInt(Integer::parseInt)
+                                              .iterator();
 
-        int result = numbers.next();
+            Iterator<Character> operations = Stream.of(question.replaceAll("What is ", "")
+                                                            .replaceAll(" by ", "")
+                                                            .split("[^plus|minus|multiplied|divided]"))
+                                                .filter(x -> x.length() > 0)
+                                                .map(x -> lexOp(x))
+                                                .iterator();
+           
+            int result = numbers.next();
 
-        if (!numbers.hasNext()) {
-            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
-        } 
-
-        while (numbers.hasNext()) {
-            result = applyOp(result, operations.next(), numbers.next());
-        }
+            if (!numbers.hasNext()) {
+                throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+            } 
         
-        return result;
+            while (numbers.hasNext()) {
+                result = applyOp(result, operations.next(), numbers.next());
+            }
+            return result;
+
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+        }
     }
 }
