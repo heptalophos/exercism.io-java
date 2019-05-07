c42e1559a1e58788e1990e0204eecdf2fb2ef7fd
Zero Delta <zerodeltahedge@gmail.com>
2019-05-07 19:22:50 +0300

    armstrong-numbers alternative solution

---
+++ armstrong-numbers/src/main/java/ArmstrongNumbers2.java
@@ -0,0 +1,14 @@
+class ArmstrongNumbers2 {
+
+	boolean isArmstrongNumber(int numberToCheck) {
+
+		int num = numberToCheck;
+		int len = String.valueOf(num).length();
+		int sum = 0;
+		while (num > 0) {
+			sum += Math.pow(num % 10, len);
+			num /= 10; 
+		}
+		return sum == numberToCheck;
+	}
+}
