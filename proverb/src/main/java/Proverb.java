36da87cf845539683d3203e4343e23a5aa5e828c
Zero Delta <zerodeltahedge@gmail.com>
2019-05-23 19:27:56 +0300

    refactor proverb

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -2,7 +2,10 @@ import java.util.stream.IntStream;
 
 public class Proverb {
 
-    private String[] words;
+    private final String[] words;
+
+    private static String premise = "For want of a %s the %s was lost.\n";
+    private static String conclusion = "And all for the want of a %s.";
 
     Proverb(String[] words) {
         this.words = words;
@@ -10,18 +13,12 @@ public class Proverb {
 
     public String recite() {
 
-        if (words.length == 0)
-            return "";
+        if (words.length == 0) 
+            return "" ;
 
-        StringBuilder proverb = new StringBuilder();
-
-        for (int i = 1; i < words.length ; i++) {
-            proverb.append(String.format("For want of a %s the %s was lost.\n", 
-                                          words[i - 1], words[i]));
-        }
-        
-        return proverb.append(String.format("And all for the want of a %s.", 
-                                            words[0])).toString();
+        return IntStream.range(1, words.length)
+                        .mapToObj(i -> String.format(premise, words[i - 1], words[i]))
+                        .reduce("", (line, acc) -> line + acc) + String.format(conclusion, words[0]) ;
     }
 
 }
