995cc46183688509b80d06993ba0bdc02dfd6992
Zero Delta <zerodeltahedge@gmail.com>
2019-05-24 15:21:00 +0300

    refactor proverb solution

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -4,7 +4,7 @@ public class Proverb {
 
     private final String[] words;
 
-    private static String premise = "For want of a %s the %s was lost.\n";
+    private static String premises = "For want of a %s the %s was lost.\n";
     private static String conclusion = "And all for the want of a %s.";
 
     Proverb(String[] words) {
@@ -16,9 +16,12 @@ public class Proverb {
         if (words.length == 0) 
             return "" ;
 
+        String epilogue = String.format(conclusion, words[0]);
+
         return IntStream.range(1, words.length)
-                        .mapToObj(i -> String.format(premise, words[i - 1], words[i]))
-                        .reduce("", (line, acc) -> line + acc) + String.format(conclusion, words[0]) ;
+                        .mapToObj(i -> String.format(premises, words[i - 1], words[i]))
+                        .reduce("", (line, stanza) -> line + stanza) + epilogue ;
     }
 
 }
+
