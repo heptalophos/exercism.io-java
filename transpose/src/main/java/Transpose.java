f76bf4c97b7954085a3d44505e7a3a4e6e1fd9cb
Zero Delta <zerodeltahedge@gmail.com>
2019-07-11 02:17:29 +0300

    transpose refactor

--- transpose/src/main/java/Transpose.java
+++ transpose/src/main/java/Transpose.java
@@ -1,26 +1,52 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+
 public class Transpose {
+
+	private static List<String> normalize(List<String> input, int i) {
+
+		List<String> rowz = input;
+
+		int len = rowz.size() - 1;
+		
+        for (; len >= 0; --len) {
+			String str = rowz.get(len);
+        	Matcher m = Pattern.compile("\\s+$").matcher(str);
+            if (m.find(i)) {
+				i = m.start();
+				rowz.set(len, str.substring(0, i));
+			} else {
+				break;
+			}
+		}
+		return rowz;
+    }
 
     public String transpose(final String input) {
 
         if (input == "") return "";
 
-        List<String> rows = Arrays.stream(input.split("\n")).collect(Collectors.toList());
+		List<String> rows = Arrays.stream(input.split("\n"))
+								  .collect(Collectors.toList());
 
-        int maxRow = rows.stream().mapToInt(String::length).max().orElse(0);
+		int maxRowLen = rows.stream()
+						 	.mapToInt(String::length)
+						 	.max()
+						 	.orElse(0);
 
-        return IntStream.range(0, maxRow)
-               .mapToObj(i -> rows.stream()
-                              .map(row -> { 
-                                       return i >= row.length() ? " " :
-                                       Character.toString(row.charAt(i)) ;
-                                  })
-                              .collect(Collectors.joining("")))
-                .collect(Collectors.joining("\n"))
-                .trim();
+		List<String> tRows = IntStream
+							 .range(0, maxRowLen)
+							 .mapToObj(i -> rows.stream()
+                                           		.map(row -> i >= row.length() ? " " :
+                                                       		Character.toString(row.charAt(i)))
+							               		.collect(Collectors.joining("")))
+							 .collect(Collectors.toList());
+							 
+		return String.join("\n", normalize(tRows, 0));
     }
 }
