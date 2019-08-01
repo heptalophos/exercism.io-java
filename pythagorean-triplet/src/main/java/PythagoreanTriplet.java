7f356a0c41e61e674bd2ebbb9c37901414a7098f
Zero Delta <zerodeltahedge@gmail.com>
2019-08-01 16:45:10 +0300

    PythagoreanTriplet 1st iteration

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,4 +1,28 @@
-public class PythagoranTriplet {
+public class PythagoreanTriplet {
 
-    
+    private final int alpha;
+    private final int bravo;
+    private final int charlie;
+
+
+    PythagoreanTriplet(int a, int b, int c) {
+        alpha = a;
+        bravo = b;
+        charlie = c;
+    }
+
+    int calculateSum() {
+        return alpha + bravo + charlie;
+    }
+
+    int calculateProduct() {
+        return alpha * bravo * charlie;
+    }
+
+    boolean isPythagorean() {
+        if (alpha * alpha + bravo * bravo == charlie * charlie)
+            return true;
+        return false;
+	}
+
 }
