02220b4ac8ce48fe6619d116909cc4531348d005
Zero Delta <zerodeltahedge@gmail.com>
2019-08-02 22:12:34 +0300

    Update PythagoreanTriplet.java

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,7 +1,8 @@
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Collectors;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.stream.IntStream;
+// import java.util.stream.Collectors;
+// import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class PythagoreanTriplet {
@@ -17,73 +18,36 @@ public class PythagoreanTriplet {
         this.charlie = c;
     }
 
-    public int calculateSum() {
-        return alpha + bravo + charlie;
-    }
+    // public int calculateSum() {
+    //     return alpha + bravo + charlie;
+    // }
 
-    public int calculateProduct() {
-        return alpha * bravo * charlie;
-    }
+    // public int calculateProduct() {
+    //     return alpha * bravo * charlie;
+    // }
 
-    public boolean isPythagorean() {
-        if (alpha * alpha + bravo * bravo == charlie * charlie)
-            return true;
-        return false;
-    }
+    // public boolean isPythagorean() {
+    //     if (alpha * alpha + bravo * bravo == charlie * charlie)
+    //         return true;
+    //     return false;
+    // }
     
     public static PythagoreanTripletGen makeTripletsList() {
         return new PythagoreanTripletGen();
     }
-
-    static class PythagoreanTripletGen {
-
-        private int factorsLTE;
-        private int factorsGTE;
-        private int sumTo;
     
-        PythagoreanTripletGen withFactorsLessThanOrEqualTo(int val) {
-            factorsLTE = val;
-            return this;
-        }
-    
-        PythagoreanTripletGen withFactorsGreaterThanOrEqualTo(int val) {
-            factorsGTE = val;
-            return this;
-        }
-    
-        PythagoreanTripletGen thatSumTo(int val) {
-            sumTo = val;
-            return this;
-        }
-    
-        public List<PythagoreanTriplet> build() {
-            return IntStream
-                   .rangeClosed(1, factorsLTE / 2)
-                   .boxed()
-                   .flatMap(a -> IntStream
-                                .rangeClosed(a + 1, (factorsLTE - a) / 2)
-                                .filter(b -> b != a)
-                                .boxed()
-                                .flatMap(b -> IntStream
-                                              .of(sumTo - (a + b))
-                                              .filter(c -> c != b)
-                                              .filter(c -> a * a + b * b == c * c)
-                                              .mapToObj(c -> new PythagoreanTriplet(a, b, c))
-                                        )
-                           )
-                   .collect(Collectors.toList());
-            // final List<PythagoreanTriplet> triplets = new ArrayList<>();
-            // for (int a = 1; a <= factorsLTE / 2; a++) 
-            //     for (int b = a + 1; b <= (factorsLTE - a) / 2; b++)
-            //         if (b != a) {
-            //             int c = sumTo - (a + b);
-            //             PythagoreanTriplet triplet = new PythagoreanTriplet(a, b, c);
-            //             if (c != b) 
-            //                 if (triplet.isPythagorean() && !triplets.contains(triplet))
-            //                     triplets.add(triplet);
-            //         }
-            // return triplets;
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PythagoreanTriplet)) return false;
+        PythagoreanTriplet other = (PythagoreanTriplet) o;
+        return alpha == other.alpha &&
+               bravo == other.bravo &&
+               charlie == other.charlie;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(alpha, bravo, charlie);
+    }
 }
