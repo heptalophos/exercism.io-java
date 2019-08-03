bd94eee5002493b873de69991b90adabfca9d5a5
Zero Delta <zerodeltahedge@gmail.com>
2019-08-03 04:22:23 +0300

    pythagorean-triplet solution

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,33 +1,19 @@
-// import java.util.List;
-// import java.util.stream.IntStream;
-// import java.util.stream.Collectors;
-// import java.util.ArrayList;
-import java.util.Objects;
-
 
 public class PythagoreanTriplet {
 
-    private final int alpha;
-    private final int bravo;
-    private final int charlie;
+    private final int sideA;
+    private final int sideB;
+    private final int sideC;
 
 
     PythagoreanTriplet(int a, int b, int c) {
-        this.alpha = a;
-        this.bravo = b;
-        this.charlie = c;
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
     }
 
-    // public int calculateSum() {
-    //     return alpha + bravo + charlie;
-    // }
-
-    // public int calculateProduct() {
-    //     return alpha * bravo * charlie;
-    // }
-
     // public boolean isPythagorean() {
-    //     if (alpha * alpha + bravo * bravo == charlie * charlie)
+    //     if (sideA * sideA + sideB * sideB == sideC * sideC)
     //         return true;
     //     return false;
     // }
@@ -37,17 +23,11 @@ public class PythagoreanTriplet {
     }
     
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PythagoreanTriplet)) return false;
-        PythagoreanTriplet other = (PythagoreanTriplet) o;
-        return alpha == other.alpha &&
-               bravo == other.bravo &&
-               charlie == other.charlie;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(alpha, bravo, charlie);
+    public boolean equals(Object that) {
+        if (that == null || !(that instanceof PythagoreanTriplet)) 
+            return false;
+        return sideA == ((PythagoreanTriplet) that).sideA &&
+               sideB == ((PythagoreanTriplet) that).sideB &&
+               sideC == ((PythagoreanTriplet) that).sideC;
     }
 }
