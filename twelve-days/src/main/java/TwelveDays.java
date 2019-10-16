52628fb45e911369404efd143420308fb57ca350
Zero Delta <zerodeltahedge@gmail.com>
2019-10-16 18:30:28 +0300

    Create TwelveDays.java

---
+++ twelve-days/src/main/java/TwelveDays.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class TwelveDays {
+    
+    private static final List<String> dayXmas = 
+                Arrays.asList("first", "second", "third", 
+                              "fourth", "fifth", "sixth",
+                              "seventh", "eighth", "ninth", 
+                              "tenth", "eleventh", "twelfth");
+
+    private static final List<String> gifts = 
+            Arrays.asList("twelve Drummers Drumming,",
+                          "eleven Pipers Piping,",
+                          "ten Lords-a-Leaping,",
+                          "nine Ladies Dancing,",
+                          "eight Maids-a-Milking,",
+                          "seven Swans-a-Swimming,",
+                          "six Geese-a-Laying,",
+                          "five Gold Rings,",
+                          "four Calling Birds,",
+                          "three French Hens,",
+                          "two Turtle Doves, and",
+                          "a Partridge in a Pear Tree.\n");
+
+    String verse(int verseNumber) {
+        return String.format("On the %s day of Christmas my true love gave to me: %s", 
+                             dayXmas.get(verseNumber - 1),
+                             IntStream.range(12 - verseNumber, 12)
+                                      .boxed()
+                                      .map(i -> gifts.get(i))
+                                      .collect(Collectors.joining(" ")));
+    }
+
+    String verses(int startVerse, int endVerse) {
+        return IntStream.rangeClosed(startVerse, endVerse)
+                        .mapToObj(this::verse)
+                        .collect(Collectors.joining("\n")); 
+    }
+    
+    String sing() {
+        return verses(1, 12);
+    }
+}
