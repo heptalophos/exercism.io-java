7e2612b85d19ce692e0d5aa04eb16c14db60bd28
Zero Delta <zerodeltahedge@gmail.com>
2020-01-28 11:32:31 +0200

    Reformat scrabble-score

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -3,18 +3,25 @@ import java.util.HashMap;
 public class Scrabble {
 
     private final String word;
-    private static final HashMap<Integer, Integer> letterScore = new HashMap<>('Z' - 'A');
+    private static final HashMap<Integer, Integer> letterScore = 
+            new HashMap<>('Z' - 'A');
 
     static {
-        "AEIOULNRST".chars().forEach(letter -> letterScore.put(letter, 1));
-        "DG".chars().forEach(letter -> letterScore.put(letter, 2));
-        "BCMP".chars().forEach(letter -> letterScore.put(letter, 3));
-        "FHVWY".chars().forEach(letter -> letterScore.put(letter, 4));
-        "K".chars().forEach(letter -> letterScore.put(letter, 5));
-        "JX".chars().forEach(letter -> letterScore.put(letter, 8));
-        "QZ".chars().forEach(letter -> letterScore.put(letter, 10));
+        "AEIOULNRST".chars().forEach(letter -> 
+                                     letterScore.put(letter, 1));
+        "DG".chars().forEach(letter -> 
+                             letterScore.put(letter, 2));
+        "BCMP".chars().forEach(letter -> 
+                               letterScore.put(letter, 3));
+        "FHVWY".chars().forEach(letter -> 
+                                letterScore.put(letter, 4));
+        "K".chars().forEach(letter -> 
+                            letterScore.put(letter, 5));
+        "JX".chars().forEach(letter -> 
+                             letterScore.put(letter, 8));
+        "QZ".chars().forEach(letter -> 
+                             letterScore.put(letter, 10));
     }
-
 
     public Scrabble(String word) {
         this.word = word;
