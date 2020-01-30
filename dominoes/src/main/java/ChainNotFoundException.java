26f13c339a8659cb335a8741fa9252c007cff6fc
Zero Delta <zerodeltahedge@gmail.com>
2020-01-31 00:03:16 +0200

    Update ChainNotFoundException.java

--- dominoes/src/main/java/ChainNotFoundException.java
+++ dominoes/src/main/java/ChainNotFoundException.java
@@ -1,4 +1,9 @@
 class ChainNotFoundException extends Exception {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 12345678L;
+
 	public ChainNotFoundException(String message) {
 		super(message);
 	}
