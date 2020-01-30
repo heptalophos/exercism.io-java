f84b93862b42fa1c482fa85076bd09378acdb451
Zero Delta <zerodeltahedge@gmail.com>
2020-01-30 14:44:41 +0200

    Update Triangle.java

--- triangle/src/main/java/Triangle.java
+++ triangle/src/main/java/Triangle.java
@@ -3,6 +3,10 @@ import java.util.stream.Stream;
 class Triangle {
 
     private final long uniqSides;
+    private final long EQUILATERAL = 1;
+    private final long ISOSCELES = 2;
+    private final long SCALENE = 3;
+
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
         boolean positiveSides = side1 > 0 && side2 > 0 && side3 > 0;
@@ -12,22 +16,23 @@ class Triangle {
         boolean degenerate = side1 + side2 == side3
                           || side2 + side3 == side1
                           || side1 + side3 == side2;
-        boolean isTriangle = positiveSides && triangleInequality && !degenerate;
-        if ( !isTriangle ) {
-            throw new TriangleException("Invalid triangle");
-        }
+
+        if (!positiveSides) throw new TriangleException("All sides of a triangle should be positive.");
+        if (!triangleInequality)  throw new TriangleException("Not valid triangle. Triangle Inequality doesn't hold.");
+        if (degenerate)  throw new TriangleException("Degenerate triangle.");
+
         uniqSides = Stream.of(side1, side2, side3).distinct().count();
     }
 
     boolean isEquilateral() {
-        return uniqSides == 1;
+        return uniqSides == EQUILATERAL;
     }
 
     boolean isIsosceles() {
-        return uniqSides <= 2;
+        return uniqSides <= ISOSCELES;
     }
 
     boolean isScalene() {
-        return uniqSides == 3;
+        return uniqSides == SCALENE;
     }
 }
