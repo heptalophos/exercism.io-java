d8dbb325e6c6cdee5a93192caddca8641bc571fe
Zero Delta <zerodeltahedge@gmail.com>
2020-02-01 01:37:25 +0200

    Update ReverseString.java

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -5,7 +5,8 @@ public class ReverseString {
     public static String reverse(String inputString) {
         return IntStream.range(0, inputString.length())
                         .map(i -> inputString.charAt(inputString.length() - i - 1))
-                        .collect(StringBuilder::new, (acc, c) -> acc.append((char)c), StringBuilder::append)
+                        .collect(StringBuilder::new, 
+                                 (acc, c) -> acc.append((char)c), StringBuilder::append)
                         .toString();
     }
 }
