5c4ebf56d09b8a8cb3a493628becb2f70934337a
Invigo <invigo.devel@gmail.com>
2020-02-01 02:05:24 +0200

    Revert "Update ReverseString.java"
    This reverts commit 317330e20a89187d6b6035d0457361063835661e.

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -3,13 +3,10 @@ import java.util.stream.IntStream;
 public class ReverseString {
    
     public static String reverse(String inputString) {
-        return IntStream
-               .range(0, inputString.length())
-               .map(i -> inputString
-                         .charAt(inputString.length() - i - 1))
-               .collect(StringBuilder::new, 
-                        (acc, c) -> acc.append((char)c), 
-                        StringBuilder::append)
-               .toString();
+        return IntStream.range(0, inputString.length())
+                        .map(i -> inputString.charAt(inputString.length() - i - 1))
+                        .collect(StringBuilder::new, 
+                                 (acc, c) -> acc.append((char)c), StringBuilder::append)
+                        .toString();
     }
 }
