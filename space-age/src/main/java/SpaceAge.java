89e1a3624f123af85547fd21d729514de1bbaeb2
Zero Delta <zerodeltahedge@gmail.com>
2020-02-08 03:17:22 +0200

    Update SpaceAge.java

--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -1,6 +1,6 @@
 public class SpaceAge {
 
-    private Long seconds;
+    private final Long seconds;
 
     public SpaceAge(Long seconds) {
         this.seconds = seconds;
@@ -10,40 +10,39 @@ public class SpaceAge {
         this.seconds = new Long(seconds);
     }
 
-    double getSeconds() {
+    public double getSeconds() {
         return this.seconds;
     }
 
-    double onEarth() {
+    public double onEarth() {
         return this.seconds / 31557600.0;
     }
 
-    double onMercury() {
+    public double onMercury() {
         return onEarth() / 0.2408467;
     }
 
-    double onVenus() {
+    public double onVenus() {
         return onEarth() / 0.61519726;
     }
 
-    double onMars() {
+    public double onMars() {
         return onEarth() / 1.8808158;
     }
 
-    double onJupiter() {
+    public double onJupiter() {
         return onEarth() / 11.862615;
     }
 
-    double onSaturn() {
+    public double onSaturn() {
         return onEarth() / 29.447498;
     }
 
-    double onUranus() {
+    public double onUranus() {
         return onEarth() / 84.016846;
     }
 
-    double onNeptune() {
+    public double onNeptune() {
         return onEarth() / 164.79132;
     }
-
 }
