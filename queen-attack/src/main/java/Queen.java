1009df3f110dbe076fcda8361000c5dc6c4304ae
Zero Delta <zerodeltahedge@gmail.com>
2020-02-08 22:16:53 +0200

    Update Queen.java

--- queen-attack/src/main/java/Queen.java
+++ queen-attack/src/main/java/Queen.java
@@ -8,10 +8,14 @@ public final class Queen {
 
     Queen(int rank, int file) throws IllegalArgumentException{
 
-        if (rank < MINPOS) throw new IllegalArgumentException("Queen position must have positive row.");
-        if (file < MINPOS) throw new IllegalArgumentException("Queen position must have positive column.");
-        if (rank > MAXPOS) throw new IllegalArgumentException("Queen position must have row <= 7.");
-        if (file > MAXPOS) throw new IllegalArgumentException("Queen position must have column <= 7.");
+        if (rank < MINPOS) 
+            throw new IllegalArgumentException("Queen position must have positive row.");
+        if (file < MINPOS) 
+            throw new IllegalArgumentException("Queen position must have positive column.");
+        if (rank > MAXPOS) 
+            throw new IllegalArgumentException("Queen position must have row <= 7.");
+        if (file > MAXPOS) 
+            throw new IllegalArgumentException("Queen position must have column <= 7.");
 
         this.file = file;
         this.rank = rank;
