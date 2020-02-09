d75789ab5a021da77e453af5a5090bed2b08abce
Invigo <invigo.devel@gmail.com>
2020-02-09 03:41:55 +0200

    Revert "Refactor Queen"
    This reverts commit 17e3320e341d262a228fae4abb173d57770ef747.

--- queen-attack/src/main/java/Queen.java
+++ queen-attack/src/main/java/Queen.java
@@ -1,17 +1,7 @@
-import java.util.Arrays;
-import java.util.List;
-
 public final class Queen {
 
     static final int MINPOS = 0;
     static final int MAXPOS = 7;
-
-    private final List<String> errors = 
-        Arrays.asList(new String[]{
-            "Queen position must have positive row.", 
-            "Queen position must have positive column.",
-            "Queen position must have row <= 7.",
-            "Queen position must have column <= 7."});
 
     private final int file;
     private final int rank;
@@ -19,13 +9,13 @@ public final class Queen {
     Queen(int rank, int file) throws IllegalArgumentException{
 
         if (rank < MINPOS) 
-            throw new IllegalArgumentException(errors.get(0));
+            throw new IllegalArgumentException("Queen position must have positive row.");
         if (file < MINPOS) 
-            throw new IllegalArgumentException(errors.get(1));
+            throw new IllegalArgumentException("Queen position must have positive column.");
         if (rank > MAXPOS) 
-            throw new IllegalArgumentException(errors.get(2));
+            throw new IllegalArgumentException("Queen position must have row <= 7.");
         if (file > MAXPOS) 
-            throw new IllegalArgumentException(errors.get(3));
+            throw new IllegalArgumentException("Queen position must have column <= 7.");
 
         this.file = file;
         this.rank = rank;
