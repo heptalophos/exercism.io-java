339598a48c390279f9bfa519aa929ad0fc1bf5a7
Invigo <invigo.devel@gmail.com>
2020-02-09 03:37:25 +0200

    Refactor QueenAttackCalculator

--- queen-attack/src/main/java/QueenAttackCalculator.java
+++ queen-attack/src/main/java/QueenAttackCalculator.java
@@ -1,16 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
 public final class QueenAttackCalculator {
 
     private final Queen white;
     private final Queen black;
+    private final List<String> errors =
+        Arrays.asList(new String[]{
+            "You must supply valid positions for both Queens.", 
+            "Queens cannot occupy the same position."});
 
     QueenAttackCalculator(Queen white, Queen black) {
 
         if (black == null || white == null) 
-            throw new IllegalArgumentException("You must supply valid positions for both Queens.");
+            throw new IllegalArgumentException(errors.get(0));
         if (white.rank() == black.rank() 
             && 
             white.file() == black.file())
-            throw new IllegalArgumentException("Queens cannot occupy the same position.");
+            throw new IllegalArgumentException(errors.get(1));
 
         this.white = white;
         this.black = black;
