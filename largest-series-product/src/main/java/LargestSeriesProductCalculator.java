2fb670ca30048520265de909fc0b1cf55d1d1291
Invigo <invigo.devel@gmail.com>
2020-02-09 03:41:44 +0200

    Revert "Refactor LargestSeriesProductCalculator"
    This reverts commit 1ddf033744bff3d689fd3099b55110a8b9f2e5a2.

--- largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,22 +1,14 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.OptionalLong;
 import java.util.stream.IntStream;
 
 class LargestSeriesProductCalculator {
 
     private String input;
-    private final List<String> errors =
-        Arrays.asList(new String[]{
-            "String to search may only contain digits.",
-            "Series length must be less than or equal to " + 
-            "the length of the string to search.",
-            "Series length must be non-negative."});
 
     LargestSeriesProductCalculator(String inputNumber) {
 
         if (!inputNumber.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException(errors.get(0));
+            throw new IllegalArgumentException("String to search may only contain digits.");
         }
 
         this.input = inputNumber;
@@ -25,22 +17,19 @@ class LargestSeriesProductCalculator {
     long calculateLargestProductForSeriesLength(int numberOfDigits) {
         
         if (numberOfDigits > input.length()) {
-            throw new IllegalArgumentException(errors.get(1));
+            throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
         }
 
         if (numberOfDigits < 0) {
-            throw new IllegalArgumentException(errors.get(2));
+            throw new IllegalArgumentException("Series length must be non-negative.");
         }
         
-        OptionalLong max = 
-            IntStream.rangeClosed(0, input.length() - numberOfDigits)
-                     .mapToLong(x -> input
-                                     .substring(x, x + numberOfDigits)
-                                     .chars()
-                                     .map(y -> y - '0')
-                                     .reduce(1, (d1, d2) -> d1 * d2))
-                     .max();    
-                     
+        OptionalLong max = IntStream.rangeClosed(0, input.length() - numberOfDigits)
+                                    .mapToLong(x -> input.substring(x, x + numberOfDigits)
+                                                         .chars()
+                                                         .map(y -> y - '0')
+                                                         .reduce(1, (d1, d2) -> d1 * d2))
+                                    .max();    
         return max.orElse(1);                                                     
     }
 }
