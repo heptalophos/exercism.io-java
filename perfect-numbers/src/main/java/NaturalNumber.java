5d7846195a62fc2eacbe206f88b92451f74eb57f
Invigo <invigo.devel@gmail.com>
2020-03-03 15:20:41 +0200

    Update NaturalNumber.java

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -3,22 +3,31 @@ import java.util.stream.IntStream;
 class NaturalNumber {
 
     private final int number;
+    private static final String exception = 
+        "You must supply a natural number (positive integer)";
 
     public NaturalNumber (int number) {
+        
         if (number <= 0) 
-            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
-        this.number = number;
+            throw new IllegalArgumentException(exception);
+        
+            this.number = number;
     }
 
     public Classification getClassification() {
+        
         final int aliquot = aliquotSum();
 
-        if (aliquot == number) return Classification.PERFECT;
-        else if (aliquot > number) return Classification.ABUNDANT;
-        else return Classification.DEFICIENT;  
+        if (aliquot == number) 
+            return Classification.PERFECT;
+        else if (aliquot > number) 
+            return Classification.ABUNDANT;
+        else 
+            return Classification.DEFICIENT;  
     }
 
     private int aliquotSum () {
+        
         return IntStream.range(1, number)
                         .filter (i -> number % i == 0)
                         .reduce(0, (a, b) -> a + b);
