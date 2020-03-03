6f97348e39f81ceeba8617c08e1f96abaa691308
Invigo <invigo.devel@gmail.com>
2020-03-03 15:25:32 +0200

    Revert "Update Atbash.java"
    This reverts commit b21c9d33d92570eeb359c7d5fade6e6e33243a9a.

--- atbash-cipher/src/main/java/Atbash.java
+++ atbash-cipher/src/main/java/Atbash.java
@@ -6,34 +6,30 @@ import java.util.stream.Collectors;
 public class Atbash {
 
     private static final List<Character> alpha = 
-        "abcdefghijklmnopqrstuvwxyz"
-        .chars()
-        .mapToObj(x -> (char) x)
-        .collect(Collectors.toList());
+        "abcdefghijklmnopqrstuvwxyz".chars()
+                                    .mapToObj(x -> (char) x)
+                                    .collect(Collectors.toList());
     
     private static final List<Character> zeta = 
-        "zyxwvutsrqponmlkjihgfedcba"
-        .chars()
-        .mapToObj(x -> (char) x)
-        .collect(Collectors.toList());
+        "zyxwvutsrqponmlkjihgfedcba".chars()
+                                    .mapToObj(x -> (char) x)
+                                    .collect(Collectors.toList());
 
     private static final Map<Character, Character> cipherMap =
-        IntStream
-        .range(0, alpha.size())
-        .boxed()
-        .collect(Collectors.toMap(alpha::get, zeta::get));
+        IntStream.range(0, alpha.size())
+                 .boxed()
+                 .collect(Collectors.toMap(alpha::get, zeta::get));
 
     private static String invert(String phrase) {
-        return phrase
-               .codePoints()
-               .map (Character::toLowerCase)
-               .filter (Character::isLetterOrDigit )
-               .map ( i -> Character.isLetter(i)   ? 
-                           cipherMap.get((char) i) : i )
-               .collect(StringBuilder::new, 
-                        StringBuilder::appendCodePoint, 
-                        StringBuilder::append)
-               .toString();
+        return phrase.codePoints()
+                     .map    ( Character::toLowerCase)
+                     .filter ( Character::isLetterOrDigit )
+                     .map ( i -> Character.isLetter(i)   ? 
+                                 cipherMap.get((char) i) : i )
+                     .collect( StringBuilder::new, 
+                               StringBuilder::appendCodePoint, 
+                               StringBuilder::append)
+                     .toString();
     }
 
     public static String encode (String phrase) {
