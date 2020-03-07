9d5a5039819d1c3c7ab35e57fac5a64800eaef17
Zero Delta <zerodeltahedge@gmail.com>
2020-03-07 23:31:52 +0200

    Update LuhnValidator.java

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,16 +1,22 @@
 public class LuhnValidator {
 
     boolean isValid(String candidate) {
+        
         int length = 0, sumLuhn = 0;
         boolean flip = false;
+        
         for (int i = candidate.length() - 1; i >= 0; i--) {
             char current = candidate.charAt(i);
-            if (current == ' ') continue;
-            if (current < '0' || current > '9') return false;
-            int digit = Integer.parseInt(Character.toString(current));
+            if (current == ' ') 
+                continue;
+            if (current < '0' || current > '9') 
+                return false;
+            int digit = 
+                Integer.parseInt(Character.toString(current));
             if (flip) {
                 digit *= 2;
-                if (digit > 9) digit -= 9;
+                if (digit > 9) 
+                    digit -= 9;
             }
             sumLuhn += digit;
             length++;
