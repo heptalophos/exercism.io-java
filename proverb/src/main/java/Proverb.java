e6adbe91b11f5064ffeda059d4e4a95c5ceaf038
Zero Delta <zerodeltahedge@gmail.com>
2020-03-08 02:01:08 +0200

    Update Proverb.java

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -4,8 +4,11 @@ public class Proverb {
 
     private final String[] words;
 
-    private static String premises = "For want of a %s the %s was lost.\n";
-    private static String conclusion = "And all for the want of a %s.";
+    private static final String premises = 
+            "For want of a %s the %s was lost.\n";
+    
+    private static final String conclusion = 
+            "And all for the want of a %s.";
 
     Proverb(String[] words) {
         this.words = words;
@@ -16,11 +19,17 @@ public class Proverb {
         if (words.length == 0) 
             return "" ;
 
-        String epilogue = String.format(conclusion, words[0]);
+        String epilogue = 
+            String.format(conclusion, words[0]);
 
-        return IntStream.range(1, words.length)
-                        .mapToObj(i -> String.format(premises, words[i - 1], words[i]))
-                        .reduce("", (line, stanza) -> line + stanza) + epilogue ;
+        return IntStream
+               .range(1, words.length)
+               .mapToObj(i -> String.format(premises, 
+                                            words[i - 1], 
+                                            words[i]))
+               .reduce("", 
+                       (line, stanza) -> line + stanza
+                      ) + epilogue ;
     }
 
 }
