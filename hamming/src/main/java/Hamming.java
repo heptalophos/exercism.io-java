d744b88baab251377bb32a998f0621957f830201
Zero Delta <zerodeltahedge@gmail.com>
2020-04-06 21:59:01 +0300

    Update Hamming.java

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -5,18 +5,18 @@ public class Hamming {
     private final int hammingDistance;
     private static final String NOT_SAME_LENGTH = 
         "leftStrand and rightStrand must be of equal length.";
-    private static final String LEFT_STR_EMPTY =    
+    private static final String LEFT_STRAND_EMPTY =    
                     "left strand must not be empty.";
-    private static final String RIGHT_STR_EMPTY =
+    private static final String RIGHT_STRAND_EMPTY =
                     "right strand must not be empty.";
 
     public Hamming(String leftStrand, String rightStrand) {
 
         if (leftStrand.isEmpty() && !rightStrand.isEmpty())
-        throw new IllegalArgumentException(LEFT_STR_EMPTY);
+        throw new IllegalArgumentException(LEFT_STRAND_EMPTY);
 
         if (rightStrand.isEmpty() && !leftStrand.isEmpty())
-        throw new IllegalArgumentException(RIGHT_STR_EMPTY);
+        throw new IllegalArgumentException(RIGHT_STRAND_EMPTY);
 
         if (leftStrand.length() != rightStrand.length())
         throw new IllegalArgumentException(NOT_SAME_LENGTH);
