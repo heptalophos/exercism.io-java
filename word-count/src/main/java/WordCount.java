b655bb3b4e2954dcdbbce0fcc1cdb531e00cbbb2
Zero Delta <zerodeltahedge@gmail.com>
2020-05-13 21:42:58 +0300

    WordCount 1st iteration

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -6,11 +7,26 @@ import java.util.stream.Stream;
 
 public class WordCount {
 
-    public Map<String, Integer> phrase(String phrase) {
-        String[] words = phrase.toLowerCase().split("\\W+");
+    public Map<String, Integer> phrase(String sentence) {
+
+        if (sentence == null || sentence.isEmpty())
+            return Collections.emptyMap();
+        
+        String[] words = sentence
+                        .replaceAll("[^\\w\\d']+", " ")
+                        .trim()
+                        .toLowerCase()
+                        .split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            String w = words[i];
+            if (w.startsWith("'") && w.endsWith("'")) 
+                words[i] = w.replaceAll("'","");
+        }
+
         return Stream
-               .of(words)
-               .collect(Collectors.groupingBy(
+              .of(words)
+              .collect(Collectors.groupingBy(
                             Function.identity(), 
                             Collectors.collectingAndThen(
                                 Collectors.counting(), 
