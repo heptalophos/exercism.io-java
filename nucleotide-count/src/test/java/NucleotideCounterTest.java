6b22a84e9184d8d4b05369dfdd93b77f1d09eb08
Zero Delta <zerodeltahedge@gmail.com>
2020-06-05 23:50:05 +0300

    Update NucleotideCounterTest.java

--- nucleotide-count/src/test/java/NucleotideCounterTest.java
+++ nucleotide-count/src/test/java/NucleotideCounterTest.java
@@ -13,42 +13,42 @@ public class NucleotideCounterTest {
         NucleotideCounter nucleotideCounter = new NucleotideCounter("");
 
         assertThat(nucleotideCounter.nucleotideCounts())
-            .containsExactlyInAnyOrderEntriesOf​(
+            .containsExactlyInAnyOrderEntriesOf(
                 Map.of('A', 0, 'C', 0, 'G', 0, 'T', 0));
     }
 
-    @Ignore("Remove to run test")
+   // @Ignore("Remove to run test")
     @Test
     public void testDnaStringHasOneNucleotide() {
         NucleotideCounter nucleotideCounter = new NucleotideCounter("G");
 
         assertThat(nucleotideCounter.nucleotideCounts())
-            .containsExactlyInAnyOrderEntriesOf​(
+            .containsExactlyInAnyOrderEntriesOf(
                 Map.of('A', 0, 'C', 0, 'G', 1, 'T', 0));
     }
 
-    @Ignore("Remove to run test")
+   // @Ignore("Remove to run test")
     @Test
     public void testRepetitiveSequenceWithOnlyGuanine() {
         NucleotideCounter nucleotideCounter = new NucleotideCounter("GGGGGGG");
 
         assertThat(nucleotideCounter.nucleotideCounts())
-            .containsExactlyInAnyOrderEntriesOf​(
+            .containsExactlyInAnyOrderEntriesOf(
                 Map.of('A', 0, 'C', 0, 'G', 7, 'T', 0));
     }
 
-    @Ignore("Remove to run test")
+   // @Ignore("Remove to run test")
     @Test
     public void testDnaStringHasMultipleNucleotide() {
         NucleotideCounter nucleotideCounter
             = new NucleotideCounter("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
 
         assertThat(nucleotideCounter.nucleotideCounts())
-            .containsExactlyInAnyOrderEntriesOf​(
+            .containsExactlyInAnyOrderEntriesOf(
                 Map.of('A', 20, 'C', 12, 'G', 17, 'T', 21));
     }
 
-    @Ignore("Remove to run test")
+   // @Ignore("Remove to run test")
     @Test
     public void testDnaStringHasInvalidNucleotides() {
         assertThrows(
