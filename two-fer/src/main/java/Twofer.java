dd4f4f942572809d94ca477317b9c8b93e792fbf
Zero Delta <zerodeltahedge@gmail.com>
2020-06-09 21:42:02 +0300

    Update Twofer.java

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,6 @@
 class Twofer {
     String twofer(String name) {
-        return String.format ("One for %s, one for me.", (name != null ? name : "you"));
+        return String.format ("One for %s, one for me.", 
+                             (name != null ? name : "you"));
     }
 }
