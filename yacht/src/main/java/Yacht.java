a17ecfc210eadda3f14185282720aa250d7514cf
Zero Delta <zerodeltahedge@gmail.com>
2021-01-22 18:35:15 +0200

    working Yacht.java

--- yacht/src/main/java/Yacht.java
+++ yacht/src/main/java/Yacht.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
@@ -8,15 +9,18 @@ class Yacht {
 
     private final YachtCategory yachtCategory;
     private final int[] dice;
-    private Map<Integer, Integer> occurenceCount;
+    private Map<Integer, Long> occurenceCount;
 
     Yacht(int[] dice, YachtCategory yachtCategory) {
         this.dice = dice;
         this.yachtCategory = yachtCategory;
         this.occurenceCount = 
-            IntStream
-            .of(this.dice)
-            .mapToObj(Integer::new)
+            // IntStream
+            // .of(this.dice)
+            // .mapToObj(Integer::new)
+            Arrays
+            .stream(dice)
+            .boxed()
             .collect(Collectors
                      .groupingBy(
                            Function.identity(),
@@ -27,42 +31,48 @@ class Yacht {
     int score() {
         switch (yachtCategory) {
             case ONES -> {
-                return (int) occurenceCount
-                             .getOrDefault(1, 0);
+                return occurenceCount
+                       .getOrDefault(1, 0L)
+                       .intValue();
             }
             case TWOS -> {
-                return (int) occurenceCount
-                             .getOrDefault(2, 0) * 2;
+                return 2 * occurenceCount
+                           .getOrDefault(2, 0L)
+                           .intValue();
             }
             case THREES -> {
-                return (int) occurenceCount
-                             .getOrDefault(3, 0) * 3;
+                return 3 * occurenceCount
+                           .getOrDefault(3, 0L)
+                           .intValue();
             }
             case FOURS -> {
-                return (int) occurenceCount
-                             .getOrDefault(4, 0) * 4;
+                return 4 * occurenceCount
+                           .getOrDefault(4, 0L)
+                           .intValue();
             }
             case FIVES -> {
-                return (int) occurenceCount
-                             .getOrDefault(5, 0) * 5;
+                return 5 * occurenceCount
+                           .getOrDefault(5, 0L)
+                           .intValue();
             }
             case SIXES -> {
-                return (int) occurenceCount
-                             .getOrDefault(6, 0) * 6;
+                return 6 * occurenceCount
+                           .getOrDefault(6, 0L)
+                           .intValue();
             }
             case CHOICE -> {
                 int res = 0;
-                for (Map.Entry<Integer, Integer> d : occurenceCount.entrySet())
+                for (Map.Entry<Integer, Long> d : occurenceCount.entrySet())
                     res += d.getKey() * d.getValue();
                 return res;
             }
             case FULL_HOUSE -> {
                 int res = 0;
                 if (occurenceCount.isEmpty() || occurenceCount.size() != 2 || 
-                    occurenceCount.containsKey(6)) {
+                    occurenceCount.containsValue(1L)) {
                         return res;
                     }
-                for (Map.Entry<Integer, Integer> d : occurenceCount.entrySet())
+                for (Map.Entry<Integer, Long> d : occurenceCount.entrySet())
                     res += d.getKey() * d.getValue();
                 return res;
             }
