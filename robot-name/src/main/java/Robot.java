ecd903ae4453991947b14fe477e0421718a1d02e
Zero Delta <zerodeltahedge@gmail.com>
2021-01-24 05:46:01 +0200

    Robot.java (1st iteration)

---
+++ robot-name/src/main/java/Robot.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static java.lang.String.format;
+
+class Robot {
+    
+    private String name;
+    private static final Set<String> used = 
+                                new HashSet<>();
+    private static final Random random = 
+                                new Random();
+
+    String getName() {
+        if (name == null) {
+            name = generateName();
+        }
+        return name;
+    }
+
+    void reset() {
+        name = null;
+    }
+
+    private static String generateName() {
+        String newName = null;
+        while (newName == null) {
+            newName = 
+                format("%s%3d", randomAlpha(2), 
+                                random.nextInt(1000));
+            if (used.contains(newName)) {
+                newName = null;
+            }
+        }
+        used.add(newName);
+        return newName;
+    }
+
+    private static String randomAlpha(int length) {
+        String randString = "";
+        for (int i = 0; i < length; i++)
+            randString += 
+                Character.toString(
+                    (char) random.nextInt(26) + ((int)'A')
+                );
+        return randString; 
+    }
+}
