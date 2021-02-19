169cae7bb11fc4e755fa2b237523595ce95302d8
Zero Delta <zerodeltahedge@gmail.com>
2021-02-19 18:47:31 +0200

    Refactor FoodChain.java

--- food-chain/src/main/java/FoodChain.java
+++ food-chain/src/main/java/FoodChain.java
@@ -1,50 +1,106 @@
-import static java.lang.String.format;
-
 public class FoodChain {
+
+    private static final String SPIDER = 
+        "wriggled and jiggled and tickled inside her";
 
     private static final String[] ANIMALS = 
         {"fly", "spider", "bird", "cat", "dog", "goat", "cow"};
-    private static final String PROLOGUE = 
-        "I know an old lady who swallowed a";
-    private static final String[] CONSEQUENCES = {
-        "It wriggled and jiggled and tickled inside her.\n",
-        "How absurd to swallow a bird!\n",
-        "Imagine that, to swallow a cat!\n",
-        "What a hog, to swallow a dog!\n",
-        "Just opened her throat and swallowed a goat!\n",
-        "I don't know how she swallowed a cow!\n",
-        "She's dead, of course!"
-    };
-    private static final String PREMISE = "She swallowed the ";
-    private static final String CONCLUSION = " to catch the ";
 
-    String verse(int n) {
-        if (n == 8) {
-            return format("%s %s.\n%s", PROLOGUE, "horse", "She's dead, of course!");
+    private static final String PROLOGUE =
+        "I know an old lady who swallowed a";
+    
+    private static final String[] PERPLEXITIES =
+        { "How absurd to swallow a bird!",
+          "Imagine that, to swallow a cat!",
+          "What a hog, to swallow a dog!",
+          "Just opened her throat and swallowed a goat!",
+          "I don\'t know how she swallowed a cow!" };
+
+    private static final String PREMISE = "She swallowed the ";
+
+    private static final String CONCLUSION = " to catch the "; 
+    
+    private static final String[] EPILOGUE = 
+        {"I don't know why she swallowed the fly. Perhaps she'll die.", 
+         "I know an old lady who swallowed a horse.\nShe's dead, of course!"};
+
+    private String firstLine(int verseNum) {
+        String line = "";
+        if (verseNum >= 1 && verseNum < 8) {
+            line += PROLOGUE + " " + ANIMALS[verseNum - 1] + ".\n";
+            if (verseNum == 2)
+                line += "It " + SPIDER + ".\n";
+        } else if (verseNum == 8) {
+            line = "";
+        } else {
+            throw new IllegalArgumentException("invalid first line");
         }
-        n--;
-        String chain = format("%s %s.\n%s", PROLOGUE, ANIMALS[n], CONSEQUENCES[n]);
-        while (n-- > 0) {
-            chain += format("%s%s%s%s.", PREMISE, ANIMALS[n+1], CONCLUSION, ANIMALS[n]);
-            if (n == 1) {
-                chain += " that wriggled and jiggled and tickled inside her.";
-            }
-            chain += "\n";
-        }
-        return chain + "I don't know why she swallowed the fly. Perhaps she'll die.";
+        return line;
     }
 
-    String verses(int start, int end) {
-		String chain = "";
+    private String secondLine(int verseNum) {
+        if (verseNum <= 1 || verseNum == 2 || verseNum >= 8) {
+            return "";
+        }
+        return PERPLEXITIES[verseNum - 3] + "\n";
+    }
 
-		while (start++ <= end)
-			chain += verse(start - 1) + "\n\n";
+    private String stemLines(int verseNum) {
+        String lines = "";
+        if (verseNum >= 2 && verseNum <= 7)
+            for (int i = verseNum; i > 1; i--) {
+                lines += PREMISE + ANIMALS[i - 1] + 
+                         CONCLUSION + ANIMALS[i - 2];
+                if (i == 3)
+                    lines += " that " + SPIDER;                
+                lines += ".\n";
+            }
+        else if (verseNum <= 1 || verseNum >= 8)
+            lines = "";
+        else
+            throw new IllegalArgumentException("invalid stem lines");
+        return lines;
+    }
 
-		return chain.trim();
-	}
+    private String closingLine(int verseNum) {
+        if (verseNum >= 1 && verseNum <= 7)
+            return EPILOGUE[0];
+        else if (verseNum == 8) 
+            return EPILOGUE[1];
+        else
+            throw new IllegalArgumentException("invalid closing line");
+    }
 
-    String wholeSong() {
+    private boolean validVerseNum (int verseNum) {
+        return verseNum >= 1 && verseNum <= 8;
+    }
+
+    private boolean validVerseRange (int start, int end) {
+        return start <= end && end >= 1 && end <= 8  &&
+               start >= 1 && start <= 8;
+    }
+
+    public String verse(int verseNum) {
+        String recite = "";
+        if (validVerseNum(verseNum))
+            recite += firstLine(verseNum) + secondLine(verseNum) +
+                      stemLines(verseNum) + closingLine(verseNum);
+        else
+            throw new IllegalArgumentException("invalid verse Num");
+        return recite.trim();
+    }
+
+    public String verses(int start, int end) {
+        String recite = "";
+        if (validVerseRange(start, end))
+            while (start++ <= end) 
+                recite += verse(start - 1) + "\n\n";
+        else
+            throw new IllegalArgumentException("invalid verse range");
+        return recite.trim();
+    }
+
+    public String wholeSong() {
         return verses(1, 8);
     }
-
 }
