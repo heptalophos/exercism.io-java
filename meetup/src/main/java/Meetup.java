40a6de95ecb9751e18f4b12556ec07236b5a1ce0
Zero Delta <zerodeltahedge@gmail.com>
2021-03-18 23:27:20 +0200

    Meetup.java 1st iteration

--- meetup/src/main/java/Meetup.java
+++ meetup/src/main/java/Meetup.java
@@ -1,9 +1,5 @@
-import java.util.stream.Stream;
 import java.time.LocalDate;
 import java.time.DayOfWeek;
-
-import static java.util.stream.IntStream.rangeClosed;
-import static java.util.stream.Collectors.toList;
 
 public class Meetup {
 
@@ -15,6 +11,37 @@ public class Meetup {
         this.year = year;
     }
 
+    private LocalDate scheduled(int wday, int first, int last) {
+        LocalDate day = LocalDate.of(year, month, first);
+        while (day.getDayOfMonth() != last + 1) {
+            if (day.getDayOfWeek().getValue() == wday) break;
+            day = day.plusDays(1);
+        }
+        return day;
+    }
 
+    public LocalDate day(DayOfWeek wday, MeetupSchedule m) {
+
+        switch (m) {
+            case FIRST: 
+                return scheduled(wday.getValue(), 1, 7);
+            case SECOND: 
+                return scheduled(wday.getValue(), 8, 14);
+            case THIRD: 
+                return scheduled(wday.getValue(), 15, 21);
+            case FOURTH: 
+                return scheduled(wday.getValue(), 22, 28);
+            case LAST:
+                int lastDay =   
+                    LocalDate.of(year, month, 1)
+                    .lengthOfMonth();
+                return scheduled(wday.getValue(), lastDay - 6, lastDay);
+            case TEENTH: 
+                return scheduled(wday.getValue(), 13, 19);
+            default:
+                throw 
+                    new IllegalArgumentException("something went wrong");
+        }
+    }
 
 }
