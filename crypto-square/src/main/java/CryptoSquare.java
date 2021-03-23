3e130dcdf99d8b99d9111c942adb436eb9737367
Zero Delta <zerodeltahedge@gmail.com>
2021-03-23 23:39:51 +0200

    Update CryptoSquare.java

--- crypto-square/src/main/java/CryptoSquare.java
+++ crypto-square/src/main/java/CryptoSquare.java
@@ -1,10 +1,41 @@
-/*
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+import java.util.stream.IntStream;
 
-Please remove this comment when submitting your solution.
+public class CryptoSquare {
+    
+    private final String plaintext;
 
-*/
+    public Crypto(String plaintext) {
+        this.plaintext = plaintext;
+    }
+
+    
+    private String sanitizedPlaintext() {
+        return plaintext
+               .toLowerCase()
+               .chars()
+               .mapToObj(x -> (char) x)
+               .filter(Character::isLetterOrDigit)
+               .map(Object::toString)
+               .collect(joining());
+    }
+
+    private int squareSize() {
+        return (int) ~~(Math.sqrt(sanitizedPlaintext().length()));
+    }
+
+    private static List<String> nub(String text, int chunkSize) {
+        // int chunks = (int) ~~(text.length() / (double) chunkSize);
+        return IntStream
+               .range(0, (int) ~~(text.length() / (double) chunkSize))
+               .mapToObj(x -> text.substring(x * chunkSize, 
+                                             Math.min(text.length(), 
+                                                      x * chunkSize + chunkSize)))  
+               .collect(toList());
+    }
+
+    
+
+}
