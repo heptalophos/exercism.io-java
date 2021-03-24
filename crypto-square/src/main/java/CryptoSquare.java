92dfa6ae6bb695d93493f6d3d0ceb8d2fb308762
Zero Delta <zerodeltahedge@gmail.com>
2021-03-24 22:50:55 +0200

    Update CryptoSquare.java

--- crypto-square/src/main/java/CryptoSquare.java
+++ crypto-square/src/main/java/CryptoSquare.java
@@ -6,35 +6,52 @@ import java.util.stream.IntStream;
 public class CryptoSquare {
     
     private final String plaintext;
+    private int squareSize;
+    private List<String> chunks;
 
     public Crypto(String plaintext) {
-        this.plaintext = plaintext;
+        plaintext = plaintext
+                    .toLowerCase()
+                    .chars()
+                    .mapToObj(x -> (char) x)
+                    .filter(Character::isLetterOrDigit)
+                    .map(Object::toString)
+                    .collect(joining());
+        squareSize = 
+            (int) ~~(Math.sqrt(plaintext.length()));
+        chunks = 
+            Arrays
+            .asList(plaintext
+                    .split("(?<=\\G.{" + squareSize + "})"));
     }
+
+    // Custom Collectors
+
 
     
-    private String sanitizedPlaintext() {
-        return plaintext
-               .toLowerCase()
-               .chars()
-               .mapToObj(x -> (char) x)
-               .filter(Character::isLetterOrDigit)
-               .map(Object::toString)
-               .collect(joining());
-    }
+    // private String sanitizedPlaintext() {
+    //     return plaintext
+    //            .toLowerCase()
+    //            .chars()
+    //            .mapToObj(x -> (char) x)
+    //            .filter(Character::isLetterOrDigit)
+    //            .map(Object::toString)
+    //            .collect(joining());
+    // }
 
-    private int squareSize() {
-        return (int) ~~(Math.sqrt(sanitizedPlaintext().length()));
-    }
+    // private int squareSize() {
+    //     return (int) ~~(Math.sqrt(sanitizedPlaintext().length()));
+    // }
 
-    private static List<String> nub(String text, int chunkSize) {
-        // int chunks = (int) ~~(text.length() / (double) chunkSize);
-        return IntStream
-               .range(0, (int) ~~(text.length() / (double) chunkSize))
-               .mapToObj(x -> text.substring(x * chunkSize, 
-                                             Math.min(text.length(), 
-                                                      x * chunkSize + chunkSize)))  
-               .collect(toList());
-    }
+    // private static List<String> chunks(String text, int chunkSize) {
+    //     // int chunks = (int) ~~(text.length() / (double) chunkSize);
+    //     return IntStream
+    //            .range(0, (int) ~~(text.length() / (double) chunkSize))
+    //            .mapToObj(x -> text.substring(x * chunkSize, 
+    //                                          Math.min(text.length(), 
+    //                                                   x * chunkSize + chunkSize)))  
+    //            .collect(toList());
+    // }
 
     
 
