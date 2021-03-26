1d8e7fdecd60bc598e591e1c15e171653378d6b7
Zero Delta <zerodeltahedge@gmail.com>
2021-03-26 23:52:04 +0200

    Update Robot.java

--- robot-simulator/src/main/java/Robot.java
+++ robot-simulator/src/main/java/Robot.java
@@ -1,7 +1,11 @@
 public class Robot {
 
+    private final GridPosition position;
+    private final Orientation orientation;
+
     public Robot(GridPosition position, Orientation orientation) {
-        
+        this.position = position;
+        this.orientation = orientation;
     }
 
     public getOrientation() {
