4166b78ea5278f5b3b42d4d2714a7e487121044b
Zero Delta <zerodeltahedge@gmail.com>
2021-06-02 20:20:55 +0300

    Update KindergartenGarden.java

--- kindergarten-garden/src/main/java/KindergartenGarden.java
+++ kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -2,12 +2,28 @@ import java.util.List;
 
 class KindergartenGarden {
 
+    private static final List<String> _ROSTER = 
+        Arrays.asList(  
+            "Alice",
+            "Bob",
+            "Charlie",
+            "David",
+            "Eve",
+            "Fred",
+            "Ginny",
+            "Harriet",
+            "Ileana",
+            "Joseph",
+            "Kincaid",
+            "Larry"
+        );
+
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
     }
 
 }
