e99cf4cdc0820321122afaba642529a3872f0041
Zero Delta <zerodeltahedge@gmail.com>
2021-06-22 14:20:45 +0300

    AffineCipher.java 1st iteration

--- affine-cipher/src/main/java/AffineCipher.java
+++ affine-cipher/src/main/java/AffineCipher.java
@@ -1,10 +1,61 @@
-/*
+class AffineCipher {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private static final int EM = 26;
 
-Please remove this comment when submitting your solution.
+    private final String NOT_COPRIME =
+        "Error: keyA and alphabet size must be coprime.";
 
-*/
+    
+
+    String encode(String plaintext, int a, int b) {
+        mmi(a, EM);
+        return plaintext
+               .toLowerCase()
+               .codePoints()
+               .filter(Character::isLetterOrDigit)
+               .map(ch -> Character.isDigit(ch) ? 
+                          ch                    :
+                          (a * (ch - 'a') + b) % EM + 'a')
+                .collect(StringBuilder::new,
+                         StringBuilder::appendCodePoint,
+                         StringBuilder::append)
+                .toString()
+                .replaceAll("(?<=\\G.{5})", " $0")
+                .trim();
+    }
+
+    String decode(String ciphertext, int a, int b) {
+        int mmi = mmi(a, EM);
+        return ciphertext
+               .toLowerCase()
+               .codePoints()
+               .filter(Character::isLetterOrDigit)
+               .map(ch -> Character.isDigit(ch) ? 
+                          ch                    :
+                          mmi * (ch - 'a' + (EM * b) - b) % EM + 'a')
+                .collect(StringBuilder::new,
+                         StringBuilder::appendCodePoint,
+                         StringBuilder::append)
+                .toString();
+    }
+
+    private int mmi(int a, int n) {
+        int r0 = n; 
+        int r1 = a; 
+        int t0 = 0; 
+        int t1 = 1;
+        while (r1 != 0) {
+            int r = r1; 
+            int t = t1;
+            int q = r0 / r1; 
+            r1 = r0 - r * q; 
+            t1 = t0 - t * q; 
+            r0 = r; t0 = t;
+        }
+        if (r0 > 1)
+            throw new IllegalArgumentException(NOT_COPRIME);
+        if (t0 < 0)
+            t0 += n;
+        return t0;
+    }
+}
