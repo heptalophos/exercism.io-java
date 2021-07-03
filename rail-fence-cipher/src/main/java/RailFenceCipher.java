2ccd856f8d440c801db77c4bb4db9049b699bd59
Zero Delta <zerodeltahedge@gmail.com>
2021-07-03 14:46:20 +0300

    Update RailFenceCipher.java

--- rail-fence-cipher/src/main/java/RailFenceCipher.java
+++ rail-fence-cipher/src/main/java/RailFenceCipher.java
@@ -1,10 +1,17 @@
-/*
+public class RailFenceCipher {
+    
+    private final int rails;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    RailFenceCipher(int rails) {
+        this.rails = rails;
+    } 
 
-Please remove this comment when submitting your solution.
+    String getEncryptedData(String plain) {
+        return "";
+    }
 
-*/
+    String getDecryptedData(String cipher) {
+        return "";
+    }
+
+}
