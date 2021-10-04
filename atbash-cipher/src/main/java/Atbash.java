e296fb4617ec254bdec399692b856e48ba05841b
Zero Delta <zerodeltahedge@gmail.com>
2021-10-04 18:23:02 +0300

    Refactor atbash-cipher

--- atbash-cipher/src/main/java/Atbash.java
+++ atbash-cipher/src/main/java/Atbash.java
@@ -1,35 +1,33 @@
 import java.util.Map;
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class Atbash {
 
     private static final List<Character> alpha = 
-        "abcdefghijklmnopqrstuvwxyz"
-        .chars()
+        "abcdefghijklmnopqrstuvwxyz".chars()
         .mapToObj(x -> (char) x)
-        .collect(Collectors.toList());
+        .collect(toList());
     
     private static final List<Character> zeta = 
-        "zyxwvutsrqponmlkjihgfedcba"
-        .chars()
+        "zyxwvutsrqponmlkjihgfedcba".chars()
         .mapToObj(x -> (char) x)
-        .collect(Collectors.toList());
+        .collect(toList());
 
     private static final Map<Character, Character> cipherMap =
-        IntStream
-        .range(0, alpha.size())
-        .boxed()
-        .collect(Collectors.toMap(alpha::get, zeta::get));
+        IntStream.range(0, alpha.size())
+                 .boxed()
+                 .collect(toMap(alpha::get, zeta::get));
 
     private static String invert(String phrase) {
-        return phrase
-               .codePoints()
+        return phrase.codePoints()
                .map (Character::toLowerCase)
                .filter (Character::isLetterOrDigit )
-               .map ( i -> Character.isLetter(i)   ? 
-                           cipherMap.get((char) i) : i )
+               .map ( i -> Character.isLetter(i) 
+                           ? cipherMap.get((char) i) 
+                           : i )
                .collect(StringBuilder::new, 
                         StringBuilder::appendCodePoint, 
                         StringBuilder::append)
@@ -37,8 +35,7 @@ public class Atbash {
     }
 
     public static String encode (String phrase) {
-        return invert(phrase)
-               .replaceAll(".{5}(?=.)", "$0 ");
+        return invert(phrase).replaceAll(".{5}(?=.)", "$0 ");
     }
 
     public static String decode (String phrase) {
