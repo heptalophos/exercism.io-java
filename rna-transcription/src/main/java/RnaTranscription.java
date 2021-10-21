68bdba9b946a8cc1e31493317ffd042b65f18803
Zero Delta <zerodeltahedge@gmail.com>
2021-10-22 02:05:00 +0300

    Update RnaTranscription.java

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -2,16 +2,16 @@ public class RnaTranscription {
     
     public String transcribe(String dnaStrand) {
         
-        StringBuilder rna = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (char c : dnaStrand.toCharArray()) {
             switch (c) {
-                case 'A': rna.append('U'); break;
-                case 'G': rna.append('C'); break;
-                case 'C': rna.append('G'); break;
-                case 'T': rna.append('A'); break;
+                case 'A': sb.append('U'); break;
+                case 'G': sb.append('C'); break;
+                case 'C': sb.append('G'); break;
+                case 'T': sb.append('A'); break;
             }
         }
-        return rna.toString();
+        return sb.toString();
     }
 }
