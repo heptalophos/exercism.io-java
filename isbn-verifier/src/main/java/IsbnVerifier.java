d784544c6bbc00bd0ea7e459f13d46486bdb8504
Zero Delta <zerodeltahedge@gmail.com>
2021-11-17 02:21:41 +0200

    Update IsbnVerifier.java

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,35 +1,18 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        
         String normal = stringToVerify.replace("-", "");
-        
         int sum = 0; 
-        
         if (normal.length() != 10) { return false; }
-        
-        for (int i = 0, j = 10; 
-             i < normal.length() - 1; 
-             i++, j--) {
-            if (Character.isDigit(normal.charAt(i))) {
-                sum += 
-                    j * Character
-                        .getNumericValue(normal.charAt(i));
-            } else { 
-                return false;
-            }
+        for (int i = 0, j = 10; i < normal.length() - 1; i++, j--) {
+            if (Character.isDigit(normal.charAt(i)))
+                sum += j * Character.getNumericValue(normal.charAt(i));
+            else return false;
         }
         char checkDig = normal.charAt(normal.length() - 1);
-        if (!Character.isDigit(checkDig)) {
-            if (checkDig != 'X') {
-                return false;
-            }
-        }
-        sum += checkDig == 'X' ? 
-                            10 : 
-                            Character
-                            .getNumericValue(checkDig);
+        if (!Character.isDigit(checkDig))
+            if (checkDig != 'X') return false;
+        sum += checkDig == 'X' ? 10 : Character.getNumericValue(checkDig);
         return sum % 11 == 0;
     }
-
 }
