fcfe83de749bbea3122ba76255f2b533a081c752
Zero Delta <zerodeltahedge@gmail.com>
2021-11-23 20:01:48 +0200

    1st passing iteration - cars-assemble

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,19 @@
 public class CarsAssemble {
 
+    private final int CARS_PER_HOUR = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        int production = speed * CARS_PER_HOUR;
+        double successRate = 
+                speed <= 0 ? 0d : 
+                speed > 0 && speed <= 4 ? 1.0 : 
+                speed > 4 && speed <= 8 ? 0.9 : 
+                speed == 9 ? 0.8 : 
+                speed == 10 ? 0.77 : 0d;
+        return successRate * production;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60d);
     }
 }
