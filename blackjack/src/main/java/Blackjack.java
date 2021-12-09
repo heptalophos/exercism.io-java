5685baf221e184b8c36147c6e4265ac911626995
Zero Delta <zerodeltahedge@gmail.com>
2021-12-09 13:05:36 +0200

    Refactor Blackjack.java

--- blackjack/src/main/java/Blackjack.java
+++ blackjack/src/main/java/Blackjack.java
@@ -55,11 +55,8 @@ public class Blackjack {
     public String firstTurn(String card1, String card2, String dealerCard) {
         int handScore = parseCard(card1) + parseCard(card2);
         int dealerScore = parseCard(dealerCard);
-
-        if (20 < handScore) {
-            return largeHand(isBlackjack(card1, card2), dealerScore);
-        } else {
-            return smallHand(handScore, dealerScore);
-        }
+        return 20 < handScore ?
+               largeHand(isBlackjack(card1, card2), dealerScore) :
+               smallHand(handScore, dealerScore);
     }
 }
