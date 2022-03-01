be5e3adb738134f31981ca57c9a84a2e3d113f3d
Zero Delta <zerodeltahedge@gmail.com>
2022-03-01 23:31:00 +0200

    Update SqueakyClean.java

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,31 @@
+import javax.lang.model.util.ElementScanner14;
+
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        StringBuilder cleaned = new StringBuilder(identifier.length());
+        boolean wordBoundary = false;
+        for (char ch: identifier.toCharArray()) {
+            if (isNotGreek(ch)) {
+                if (wordBoundary) {
+                    cleaned.append(Character.toUpperCase(ch));
+                } else
+                if (Character.isWhitespace(ch)) {
+                    cleaned.append('_');
+                } else 
+                if (Character.isISOControl(ch)) {
+                    cleaned.append("CTRL");
+                } else 
+                if (Character.isLetter(ch)) {
+                    cleaned.append(ch);
+                }
+                wordBoundary = (ch == '-');
+            }
+            else continue;
+        }
+        return cleaned.toString();
+    }
+
+    private static boolean isNotGreek(char ch) {
+        return (ch < 'α' && 'ω' < ch);
     }
 }
