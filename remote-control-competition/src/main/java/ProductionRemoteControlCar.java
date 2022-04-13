69cca3ef2d726bd4e1bdaa396d1af1efc5b2199e
Zero Delta <zerodeltahedge@gmail.com>
2022-04-13 23:40:48 +0300

    Create ProductionRemoteControlCar.java

---
+++ remote-control-competition/src/main/java/ProductionRemoteControlCar.java
@@ -0,0 +1,18 @@
+class ProductionRemoteControlCar implements RemoteControlCar {
+
+    public void drive() {
+        throw new UnsupportedOperationException("");
+    }
+
+    public int getDistanceTravelled() {
+        throw new UnsupportedOperationException("");
+    }
+
+    public int getNumberOfVictories() {
+        throw new UnsupportedOperationException("");
+    }
+
+    public void setNumberOfVictories(int numberofVictories) {
+        throw new UnsupportedOperationException("");
+    }
+}
