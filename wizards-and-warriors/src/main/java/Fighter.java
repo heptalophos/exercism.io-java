bdc92589b5145cc5cc87839590796a07f21f6323
Zero Delta <zerodeltahedge@gmail.com>
2022-05-16 20:14:36 +0300

    Update Fighter.java

--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -1,52 +1,40 @@
 abstract class Fighter {
 
     boolean isVulnerable() {
-        throw new UnsupportedOperationException(
-            "Please provide implementation for this method"
-        );
+        return false;
     }
 
     abstract int damagePoints(Fighter fighter);
 
+    // @Override
+    public String toString() {
+        return "Fighter is a " + getClass().getSimpleName();
+    }
 }
 
 class Warrior extends Fighter {
 
     @Override
-    public String toString() {
-        throw new UnsupportedOperationException(
-            "Please implement the toString() method with the required text"
-        );
-    }
-
-    @Override
     int damagePoints(Fighter wizard) {
-        throw new UnsupportedOperationException(
-            "Please implement Warrior.damagePoints() method"
-        );
+        return wizard.isVulnerable() ? 10 : 6;
     }
 }
 
 class Wizard extends Fighter {
 
+    private boolean spellReady = false; 
+
     @Override
     boolean isVulnerable() {
-        throw new UnsupportedOperationException(
-            "Please implement Wizard.isVulnerable() method"
-        );
+        return spellReady == false;
     }
 
     @Override
     int damagePoints(Fighter warrior) {
-        throw new UnsupportedOperationException(
-            "Please implement Wizard.damagePoints() method"
-        );
+        return spellReady ? 12 : 3;
     }
 
     void prepareSpell() {
-        throw new UnsupportedOperationException(
-            "Please implement Wizard.prepareSpell() method"
-        );
+        spellReady = true;
     }
-
 }
