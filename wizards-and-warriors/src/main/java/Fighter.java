05caaffed427bc73d20352e84573c67cb4cf798f
Zero Delta <zerodeltahedge@gmail.com>
2022-05-16 19:38:19 +0300

    Update Fighter.java

--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -1,7 +1,9 @@
 abstract class Fighter {
 
     boolean isVulnerable() {
-        throw new UnsupportedOperationException("Please provide implementation for this method");
+        throw new UnsupportedOperationException(
+            "Please provide implementation for this method"
+        );
     }
 
     abstract int damagePoints(Fighter fighter);
@@ -12,12 +14,16 @@ class Warrior extends Fighter {
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Please implement the toString() method with the required text");
+        throw new UnsupportedOperationException(
+            "Please implement the toString() method with the required text"
+        );
     }
 
     @Override
     int damagePoints(Fighter wizard) {
-        throw new UnsupportedOperationException("Please implement Warrior.damagePoints() method");
+        throw new UnsupportedOperationException(
+            "Please implement Warrior.damagePoints() method"
+        );
     }
 }
 
@@ -25,16 +31,22 @@ class Wizard extends Fighter {
 
     @Override
     boolean isVulnerable() {
-        throw new UnsupportedOperationException("Please implement Wizard.isVulnerable() method");
+        throw new UnsupportedOperationException(
+            "Please implement Wizard.isVulnerable() method"
+        );
     }
 
     @Override
     int damagePoints(Fighter warrior) {
-        throw new UnsupportedOperationException("Please implement Wizard.damagePoints() method");
+        throw new UnsupportedOperationException(
+            "Please implement Wizard.damagePoints() method"
+        );
     }
 
     void prepareSpell() {
-        throw new UnsupportedOperationException("Please implement Wizard.prepareSpell() method");
+        throw new UnsupportedOperationException(
+            "Please implement Wizard.prepareSpell() method"
+        );
     }
 
 }
