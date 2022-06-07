07eba11d66f4b9cc8be4e10f1d2993f6842cd559
Zero Delta <zerodeltahedge@gmail.com>
2022-06-07 21:39:05 +0300

    FootballMatchReports.java, 1st passing iteration

--- football-match-reports/src/main/java/FootballMatchReports.java
+++ football-match-reports/src/main/java/FootballMatchReports.java
@@ -1,5 +1,25 @@
 public class FootballMatchReports {    
     public static String onField(int shirtNum) {
-        throw new UnsupportedOperationException("Please implement the (static) FootballMatchReports.onField() method");
+        // only works with java9 and up
+        return switch(shirtNum) {
+            case 1 -> 
+                "goalie";
+            case 2 -> 
+                "left back";
+            case 3, 4 -> 
+                "center back";
+            case 5 -> 
+                "right back";
+            case 6, 7, 8 -> 
+                "midfielder";
+            case 9 -> 
+                "left wing";
+            case 10 -> 
+                "striker";
+            case 11 -> 
+                "right wing";
+            default -> 
+                throw new IllegalArgumentException("invalid position");
+        };
     }
 }
