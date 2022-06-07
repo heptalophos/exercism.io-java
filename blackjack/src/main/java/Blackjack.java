2b150c6eb4b733b1faba5a862143ce54a515ce15
Zero Delta <zerodeltahedge@gmail.com>
2022-06-07 21:08:21 +0300

    Refactor blackjack

--- blackjack/src/main/java/Blackjack.java
+++ blackjack/src/main/java/Blackjack.java
@@ -8,32 +8,19 @@ public class Blackjack {
 
     public int parseCard(String card) {
         int cardScore = 
-            card.equalsIgnoreCase("ace")   ?
-            11 :
-            card.equalsIgnoreCase("two")   ?
-            2  :
-            card.equalsIgnoreCase("three") ?
-            3  :
-            card.equalsIgnoreCase("four")  ?
-            4  :
-            card.equalsIgnoreCase("five")  ?
-            5  :
-            card.equalsIgnoreCase("six")   ?
-            6  :
-            card.equalsIgnoreCase("seven") ?
-            7  :
-            card.equalsIgnoreCase("eight") ?
-            8  :
-            card.equalsIgnoreCase("nine")  ?
-            9  :
-            card.equalsIgnoreCase("ten")   ?
-            10 :
-            card.equalsIgnoreCase("jack")  ?
-            10 :
-            card.equalsIgnoreCase("queen") ?
-            10 :
-            card.equalsIgnoreCase("king")  ?
-            10 : 0;
+            card.equalsIgnoreCase("ace")   ?  11 :
+            card.equalsIgnoreCase("two")   ?  2  :
+            card.equalsIgnoreCase("three") ?  3  :
+            card.equalsIgnoreCase("four")  ?  4  :
+            card.equalsIgnoreCase("five")  ?  5  :
+            card.equalsIgnoreCase("six")   ?  6  :
+            card.equalsIgnoreCase("seven") ?  7  :
+            card.equalsIgnoreCase("eight") ?  8  :
+            card.equalsIgnoreCase("nine")  ?  9  :
+            card.equalsIgnoreCase("ten")   ?  10 :
+            card.equalsIgnoreCase("jack")  ?  10 :
+            card.equalsIgnoreCase("queen") ?  10 :
+            card.equalsIgnoreCase("king")  ?  10 : 0;
         return cardScore;                
     }
 
@@ -55,11 +42,8 @@ public class Blackjack {
     public String firstTurn(String card1, String card2, String dealerCard) {
         int handScore = parseCard(card1) + parseCard(card2);
         int dealerScore = parseCard(dealerCard);
-
-        if (20 < handScore) {
-            return largeHand(isBlackjack(card1, card2), dealerScore);
-        } else {
-            return smallHand(handScore, dealerScore);
-        }
+        return (20 < handScore)  
+               ? largeHand(isBlackjack(card1, card2), dealerScore) 
+               : smallHand(handScore, dealerScore);
     }
 }
