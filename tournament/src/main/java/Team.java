b9b3e2cf2704ab129c5f5e579629834506854bdd
Zero Delta <zerodeltahedge@gmail.com>
2022-09-20 19:14:24 +0300

    Update Team.java

--- tournament/src/main/java/Team.java
+++ tournament/src/main/java/Team.java
@@ -27,4 +27,19 @@ public class Team implements Comparable<Team> {
         return 3 * wins + draws;
     }
 
+    void win() {
+        wins += 1;
+    }
+    void draw() {
+        draws += 1;
+    }
+    void lose() {
+        losses += 1;
+    }
+
+    private int compare(Team this, Team that) {
+        int rank = Integer.compare(that.getPoints(), this.getPoints());
+        return rank == 0 ? this.getTeam().compareTo(that.getTeam())
+                         : rank;
+    }
 }
