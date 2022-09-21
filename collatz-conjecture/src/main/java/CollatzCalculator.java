66a4ce8dd9fb1bea0c706522d0f6544cdcdcabf9
Zero Delta <zerodeltahedge@gmail.com>
2022-09-21 21:46:47 +0300

    Refactor CollatzCalculator.java

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,17 +1,23 @@
 class CollatzCalculator {
-    private static final String ERR = 
+    private static final String ERROR = 
         "Only natural numbers are allowed";
 
     public int computeStepCount(final int start) {
         if (start <= 0) 
-            throw new IllegalArgumentException(ERR);
+            throw new IllegalArgumentException(ERROR);
         return computeStepCount(start, 0);
     }
 
     private int computeStepCount(int start, int steps) {
         if (start == 1) return steps;
         steps += 1;
-        start = start % 2 == 1 ? (3 * start) + 1 : start / 2;
+        if (start % 2 == 1) {
+            start *= 3;
+            start += 1;
+        }
+        else {
+            start /= 2;
+        }
         return computeStepCount(start, steps);
     }
 }
