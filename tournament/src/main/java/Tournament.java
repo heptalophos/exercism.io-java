dac51526c1918475d95d064d7159b2b0b0a9fcd3
Zero Delta <zerodeltahedge@gmail.com>
2022-09-21 20:46:00 +0300

    Update Tournament.java

--- tournament/src/main/java/Tournament.java
+++ tournament/src/main/java/Tournament.java
@@ -1,51 +1,53 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Tournament {
     private static final String MATCHES_FORMAT = 
         "%-30s | %2s | %2s | %2s | %2s | %2s\n";
-    private Map<String, Team> teams = new HashMap<>();
+    private Map<String, Team> roster = new HashMap<>();
 
     void applyResults(String matchesPlayed) {
         for (String match: matchesPlayed.split("\n")) {
-            String data[] = match.split(";");
-            if (data.length != 3) {
-                throw new IllegalArgumentException("Invalid match data");
+            List<String> data = Arrays.asList(match.split(";"));
+
+            if (data.size() != 3) {
+                throw new IllegalArgumentException("Invalid match record");
             }
-            Team home = teams.computeIfAbsent(data[0], Team::new);
-            Team away = teams.computeIfAbsent(data[1], Team::new);
-            String outcome = data[2].toUpperCase();
+            Team home = roster.computeIfAbsent(data.get(0), Team::new);
+            Team away = roster.computeIfAbsent(data.get(1), Team::new);
+            String outcome = data.get(2);
 
             switch(outcome) {
-                case "WIN": home.win(); away.lose(); break;
-                case "LOSS": home.lose(); away.win(); break;
-                case "DRAW": home.draw(); away.draw(); break;
+                case "win" : home.win();  away.lose(); break;
+                case "loss": home.lose(); away.win();  break;
+                case "draw": home.draw(); away.draw(); break;
                 default: throw new UnsupportedOperationException(outcome);
             }
         }
     }
     
     String printTable() {
-        String header = 
-            String.format(MATCHES_FORMAT, "Team", "MP", "W", "D", "L", "P");
+        List<Team> teams = new ArrayList<>(roster.values());
+        teams.sort(Team::compareTo);
+        
         StringBuilder table = new StringBuilder();
+        String header = String.format(MATCHES_FORMAT, 
+                                      "Team", "MP", "W", "D", "L", "P");
         table.append(header);
-        List<Team> teamz = new ArrayList<>(teams.values());
-        Collections.sort(teamz);
-        for (Team t: teamz) {
-            String teamRow = String.format( MATCHES_FORMAT, 
-                                            t.getTeam(), 
-                                            t.getMatches(),
-                                            t.getWins(), 
-                                            t.getDraws(),
-                                            t.getLosses(), 
-                                            t.getPoints() );
-            table.append(teamRow);
+
+        for (Team team: teams) {
+            String teamLine = String.format(MATCHES_FORMAT, 
+                                            team.getTeam(), 
+                                            team.getMatches(),
+                                            team.getWins(), 
+                                            team.getDraws(),
+                                            team.getLosses(), 
+                                            team.getPoints());
+            table.append(teamLine);
         } 
         return table.toString();
-
     }
 }
