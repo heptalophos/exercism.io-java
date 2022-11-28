5fd62812cd93d9cbce8ca3f9b1195066820aeafc
Zero Delta <zerodeltahedge@gmail.com>
2022-11-28 14:40:29 +0200

    Update Domino.java

--- dominoes/src/main/java/Domino.java
+++ dominoes/src/main/java/Domino.java
@@ -23,27 +23,20 @@ class Domino {
     
     @Override
     public boolean equals(Object o) {
-        
         Domino that = (Domino) o;
-        
-        return ( this.getLeft() == that.getLeft() 
-                 && 
-                 this.getRight() == that.getRight()) 
-               ||
-               ( this.getLeft() == that.getRight() 
-                 && 
-                 this.getRight() == that.getLeft());
+        return ( this.getLeft() == that.getLeft() && 
+                 this.getRight() == that.getRight() ) ||
+               ( this.getLeft() == that.getRight() && 
+                 this.getRight() == that.getLeft() );
     }
     
     @Override
     public int hashCode() {
-        
         return Objects.hash(left, right);
     }
 
     @Override
 	public String toString() {
-        
         return "[" + this.getLeft() + "|" + this.getRight() + "]";
 	}
 }
