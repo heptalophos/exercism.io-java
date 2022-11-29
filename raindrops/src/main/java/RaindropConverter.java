6a2b5354fd9f922211af0d026a605abdfc319554
Zero Delta <zerodeltahedge@gmail.com>
2022-11-29 22:31:08 +0200

    Reformat RaindropConverter.java
    raindrops\src\main\java\RaindropConverter.java

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,18 +1,13 @@
 public class RaindropConverter {
 
     public static String convert(int number) {
-        StringBuilder drops = 
-                new StringBuilder() ;
+        StringBuilder drops = new StringBuilder() ;
 
         drops.append(number % 3 == 0 ? "Pling" : "");
         drops.append(number % 5 == 0 ? "Plang" : "");
         drops.append(number % 7 == 0 ? "Plong" : "");
 
-        drops = drops.length() == 0 ? 
-                drops.append(number) : 
-                drops;
-                
+        drops = drops.length() == 0 ? drops.append(number) : drops;            
         return drops.toString();
     }
-
 }
