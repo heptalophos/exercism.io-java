a5d1e40cf66c41a1f378e206837c9f030dbe58de
Zero Delta <zerodeltahedge@gmail.com>
2023-07-28 22:17:05 +0300

    Update MatrixCoordinate.java

--- saddle-points/src/main/java/MatrixCoordinate.java
+++ saddle-points/src/main/java/MatrixCoordinate.java
@@ -1,28 +1,34 @@
 class MatrixCoordinate {
-  private final int row;
-  private final int col;
-  MatrixCoordinate(final int row, final int col) {
-    this.row = row;
-    this.col = col;
-  }
-  public int row() {
-      return this.row;
-  }
-  public int col() {
-      return this.col;
-  }
-  // Generated equals and hashcode.
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    final MatrixCoordinate that = (MatrixCoordinate) o;
-    return row == that.row && col == that.col;
-  }
-  @Override
-  public int hashCode() {
-    int result = row;
-    result = 31 * result + col;
-    return result;
-  }
+    private final int row;
+    private final int col;
+    
+    MatrixCoordinate(final int row, final int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int row() {
+        return this.row;
+    }
+
+    public int col() {
+        return this.col;
+    }
+  
+    // Generated equals and hashcode.
+  
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final MatrixCoordinate that = (MatrixCoordinate) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + col;
+        return result;
+    }
 }
