27b64d91bf2ded85bccc7c5df019799f8fed08c6
Zero Delta <zerodeltahedge@gmail.com>
2023-07-28 22:17:20 +0300

    Update Matrix.java

--- saddle-points/src/main/java/Matrix.java
+++ saddle-points/src/main/java/Matrix.java
@@ -1,56 +1,58 @@
 import java.util.List;
 import java.util.Set;
-import static java.util.Collections.emptySet;
-import static java.util.stream.IntStream.range;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
-public class Matrix {
+class Matrix {
     private final List<List<Integer>> matrix;
-    private List<Integer> maxRow;
-    private List<Integer> minCol;
-
+    private List<Integer> rowMax;
+    private List<Integer> colMin;
+    
     Matrix(List<List<Integer>> values) {
-        this.matrix = values;
+        matrix = values;
     }
-
+    
     public Set<MatrixCoordinate> getSaddlePoints() {
         if (!matrix.isEmpty()) {
-            maxRow = matrix.stream()
+            rowMax = matrix.stream()
                            .map(val -> val.stream()
                                           .max(Integer::compareTo)
-                                          .get()
-                            )
-                           .collect(toList());
-            
-            minCol = range(0, matrix.get(0).size())
-                    .mapToObj(this::getRow)
-                    .collect(toList())
-                    .stream()
-                    .map(val -> val.stream()
-                                   .min(Integer::compareTo)
-                                   .get())
-                    .collect(toList());          
-            
-            return   range(0, matrix.size())
-                    .boxed()
-                    .flatMap(r -> range(0, matrix.get(0).size())
-                                 .mapToObj(x -> 
-                                    new MatrixCoordinate(r + 1, x + 1)
-                                 )
-                            )
-                    .filter(this::saddlePoint)
-                    .collect(toSet());
+                                          .get())
+                           .collect(Collectors.toList());
+            colMin = IntStream
+                     .range(0, matrix.get(0).size())
+                     .mapToObj(this::getRow)
+                     .collect(Collectors.toList())
+                     .stream()
+                     .map(val -> val.stream()
+                                    .min(Integer::compareTo)
+                                    .get())
+                     .collect(Collectors.toList());           
+            return IntStream
+                   .range(0, matrix.size())
+                   .boxed()
+                   .flatMap(r -> IntStream
+                                 .range(0, matrix.get(0).size())
+                                 .mapToObj(x -> new MatrixCoordinate(r + 1, x + 1)))
+                                 .filter(this::saddlePoint)
+                                 .collect(Collectors.toSet());
         }
-        return emptySet();
+        return Collections.emptySet();
     }
+    
     private List<Integer> getRow(int row) {
         return matrix.stream()
                      .map(r -> r.get(row))
-                     .collect(toList());
+                     .collect(Collectors.toList());
     }
+    
     private boolean saddlePoint(MatrixCoordinate p) {
-        return matrix.get(p.row()).get(p.col()) >= maxRow.get(p.row())
-            && matrix.get(p.row()).get(p.col()) <= minCol.get(p.col());
+        return matrix.get((int) p.row())
+                     .get((int) p.col()) >= rowMax.get((int) p.row())
+               && 
+               matrix.get((int) p.row())
+                     .get((int) p.col()) <= colMin.get((int) p.col());
+        // return maxRow.get(p.row()) == minCol.get(p.col());
     } 
 }
