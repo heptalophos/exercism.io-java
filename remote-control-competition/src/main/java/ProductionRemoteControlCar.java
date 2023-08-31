3db73949f8dcc26debe13133ee4315a947f7c395
Zero Delta <zerodeltahedge@gmail.com>
2023-09-01 02:10:18 +0300

    Update ProductionRemoteControlCar.java

--- remote-control-competition/src/main/java/ProductionRemoteControlCar.java
+++ remote-control-competition/src/main/java/ProductionRemoteControlCar.java
@@ -1,16 +1,25 @@
-class ProductionRemoteControlCar implements RemoteControlCar {
+class ProductionRemoteControlCar implements RemoteControlCar, 
+                                 Comparable<ProductionRemoteControlCar>  
+{
     private int distance  = 0;
     private int victories = 0;
+    @Override
     public void drive() {
         distance += 10;
     }
+    @Override
     public int getDistanceTravelled() {
-        return distance;
+        return this.distance;
     }
+
     public int getNumberOfVictories() {
-        return victories;
+        return this.victories;
     }
     public void setNumberOfVictories(int numberofVictories) {
-        victories = numberofVictories;
+        this.victories = numberofVictories;
+    }
+    @Override
+    public int compareTo(ProductionRemoteControlCar car) {
+        return car.getNumberOfVictories() - this.getNumberOfVictories();
     }
 }
