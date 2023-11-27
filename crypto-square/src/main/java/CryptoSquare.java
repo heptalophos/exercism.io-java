0919a4e099d535242c5220d3344e7bd871957a8a
Zero Delta <zerodeltahedge@gmail.com>
2023-11-27 02:42:37 +0200

    Update CryptoSquare.java

--- crypto-square/src/main/java/CryptoSquare.java
+++ crypto-square/src/main/java/CryptoSquare.java
@@ -1,9 +1,7 @@
 import java.util.stream.IntStream;
 import java.util.Arrays;
 import java.util.List;
-
 import static java.util.stream.Collectors.joining;
-
 
 public class CryptoSquare {
     
@@ -12,28 +10,21 @@ public class CryptoSquare {
     private List<String> chunks;
 
     public CryptoSquare(String input) {
-
-        plaintext = input.toLowerCase().chars()
-                         .mapToObj(x -> (char) x)
+        plaintext = input.toLowerCase().chars().mapToObj(x -> (char) x)
                          .filter(Character::isLetterOrDigit)
-                         .map(Object::toString)
-                         .collect(joining());
-        
-        squareSize = 
-            (int) Math.ceil((double) Math.sqrt(plaintext.length()));
-        
-        chunks = 
-            Arrays.asList(plaintext.split("(?<=\\G.{" + squareSize + "})"));
+                         .map(Object::toString).collect(joining());
+        squareSize = (int) Math.ceil((double) Math.sqrt(plaintext.length()));
+        chunks = Arrays.asList(plaintext.split("(?<=\\G.{" + 
+                                                squareSize + 
+                                               "})"));
     }
-
     public String getCiphertext() {
         return IntStream.range(0, squareSize)
-                        .mapToObj(x -> 
-                            chunks.stream()
-                                  .map(s -> s.length() > x ? 
-                                            "" + s.charAt(x) : 
-                                            " ")
-                                  .collect(joining()))
-                        .collect(joining(" "));       
+               .mapToObj(x -> chunks.stream()
+                                    .map(s -> s.length() > x ? 
+                                              "" + s.charAt(x) : 
+                                              " ")
+                                    .collect(joining()))
+               .collect(joining(" "));       
     }
 }
