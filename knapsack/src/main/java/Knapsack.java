6928055f409dad33f8c9043934d905307a1bd099
Zero Delta <zerodeltahedge@gmail.com>
2023-11-30 01:29:09 +0200

    Update Knapsack.java

--- knapsack/src/main/java/Knapsack.java
+++ knapsack/src/main/java/Knapsack.java
@@ -6,13 +6,16 @@ public class Knapsack {
     public int maximumValue(final int capacity, final List<Item> items) {
         return IntStream.range(0, items.size())
                .filter(x -> items.get(x).getWeight() <= capacity)
-               .map(x -> { Item item = items.get(x);
-                           int itemValue = item.getValue();
-                           int itemWeight = item.getWeight();
-                           List<Item> remainingItems = 
-                                items.subList(x + 1, items.size());
-                           return itemValue + maximumValue(capacity - itemWeight, 
-                                                           remainingItems);
-                    }).max().orElse(0);
+               .map(x -> { 
+                    Item item = items.get(x);
+                    int itemValue = item.getValue();
+                    int itemWeight = item.getWeight();
+                    List<Item> remainingItems = 
+                            items.subList(x + 1, items.size());
+                    return  itemValue + 
+                            maximumValue(capacity - itemWeight, remainingItems);
+                })
+                .max()
+                .orElse(0);
     }
 }
