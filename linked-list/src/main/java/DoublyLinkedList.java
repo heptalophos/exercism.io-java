a784d6ec75f31f9d20e1192fba3ec5169e5d550e
Zero Delta <zerodeltahedge@gmail.com>
2023-12-08 17:11:20 +0200

    Update DoublyLinkedList.java

--- linked-list/src/main/java/DoublyLinkedList.java
+++ linked-list/src/main/java/DoublyLinkedList.java
@@ -1,29 +1,52 @@
 class DoublyLinkedList<T> {
     private Element<T> head;
+    private Element<T> tail;
 
     void push(T value) {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.push() method.");
+        Element<T> elem = new Element<>(value);
+        if (tail == null) { head = elem; }
+        else {
+            elem.setPrev(tail);
+            tail.setNext(elem);
+        }
+        tail = elem;
     }
 
     T pop() {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.pop() method.");
+        if (tail == null) return null;
+        Element<T> curr = tail;
+        if (curr.getPrev() == null) {
+            tail = null;
+            head = null;
+        }
+        else {
+            curr.getPrev().setNext(null);
+            tail = curr.getPrev();
+        }
+        return curr.getValue();
     }
 
     void unshift(T value) {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.unshift() method.");
+        Element<T> elem = new Element<>(value);
+        if (head == null) { tail = elem; }
+        else {
+            elem.setNext(head);
+            head.setPrev(elem);
+        }
+        head = elem;
     }
 
     T shift() {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.shift() method.");
-    }
-
-    private static final class Element<T> {
-        private final T value;
-        private Element<T> prev;
-        private Element<T> next;
-
-        Element(T value, Element<T> prev, Element<T> next) {
-            throw new UnsupportedOperationException("Please implement the Element constructor.");
+        if (head == null) return null;
+        Element<T> curr = head;
+        if (curr.getNext() == null) {
+            head = null;
+            tail = null;
         }
+        else {
+            curr.getNext().setPrev(null);
+            head = curr.getNext();
+        }
+        return curr.getValue();
     }
 }
