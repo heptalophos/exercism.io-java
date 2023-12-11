56b09a3b1701c3b33f4c051dabe1e2e4d30e18e3
Zero Delta <zerodeltahedge@gmail.com>
2023-12-11 19:21:57 +0200

    Update LogLine.java

--- logs-logs-logs/src/main/java/LogLine.java
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,21 @@
 public class LogLine {
+    private final String logLine;
 
     public LogLine(String logLine) {
+        this.logLine = logLine;
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+        int codixEnd = logLine.indexOf(':') - 1;
+        String codix = logLine.substring(1, codixEnd);
+        return LogLevel.codixToLevel(codix);
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        LogLevel level = this.getLogLevel();
+        int lvl = level.getLevel(), 
+            msgStart = logLine.indexOf(':') + 2;
+        String msg = logLine.substring(msgStart);
+        return "%d:%s".formatted(lvl, msg);
     }
 }
