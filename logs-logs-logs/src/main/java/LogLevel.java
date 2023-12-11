6831c8257405222979a756830d0c98b4877e1ee3
Zero Delta <zerodeltahedge@gmail.com>
2023-12-11 19:21:49 +0200

    Update LogLevel.java

--- logs-logs-logs/src/main/java/LogLevel.java
+++ logs-logs-logs/src/main/java/LogLevel.java
@@ -1,3 +1,26 @@
 public enum LogLevel {
-    // TODO: define members for each log level
+    TRACE   (1,  "TRC"),
+    DEBUG   (2,  "DBG"),
+    INFO    (4,  "INF"),
+    WARNING (5,  "WRN"),
+    ERROR   (6,  "ERR"),
+    FATAL   (42, "FTL"),
+    UNKNOWN (0,  "___");
+
+    private final int level;
+    private final String codix;
+
+    LogLevel(int level, String codix) {
+        this.level = level;
+        this.codix = codix;
+    }
+
+    public int getLevel() { return level; }
+    public String getCodix() { return codix; }
+
+    public static LogLevel codixToLevel(String codix) {
+        for (LogLevel level : values())
+            if(codix.equals(level.getCodix())) return level;
+        return LogLevel.UNKNOWN;
+    }
 }
