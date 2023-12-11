8c71b14687aec5cdfe3d86f54565c805fb24fe76
Zero Delta <zerodeltahedge@gmail.com>
2023-12-11 16:37:42 +0200

    Update DoublyLinkedList.java

--- linked-list/src/main/java/DoublyLinkedList.java
+++ linked-list/src/main/java/DoublyLinkedList.java
@@ -1,4 +1,4 @@
-class DoublyLinkedList<T> {
+public class DoublyLinkedList<T> {
     private Element<T> head;
     private Element<T> tail;
 
@@ -50,21 +50,5 @@ class DoublyLinkedList<T> {
         return curr.getValue();
     }
 
-    private static final class Element<T> {
-        private final T value;
-        private Element<T> prev;
-        private Element<T> next;
-    
-        Element(T value) {
-            this.value = value;
-            this.next = null;
-            this.prev = null;
-        }
-    
-        T getValue() { return value; }
-        Element<T> getNext() { return next; }
-        Element<T> getPrev() { return prev; }
-        void setNext(Element<T> element) { this.next = element; }
-        void setPrev(Element<T> element) { this.prev = element; }
-    }
+
 }
