bbff2bbc0d7407c445ae8de06d5550013a574e14
Zero Delta <zerodeltahedge@gmail.com>
2024-02-22 21:52:28 +0200

    Update Series.java

--- series/src/main/java/Series.java
+++ series/src/main/java/Series.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
@@ -7,11 +8,9 @@ public class Series {
 
     public final String numberString;
     
-    private static final String TOO_SMALL = 
-                            "Slice size is too small.";
+    private static final String TOO_SMALL = "Slice size is too small.";
 
-    private static final String TOO_BIG = 
-                            "Slice size is too big.";
+    private static final String TOO_LARGE = "Slice size is too big.";
 
     public Series(String numberString){
         this.numberString = numberString;
@@ -21,11 +20,10 @@ public class Series {
         if (subLen <= 0) 
             throw new IllegalArgumentException(TOO_SMALL);
         if (subLen > numberString.length()) 
-            throw new IllegalArgumentException(TOO_BIG);
+            throw new IllegalArgumentException(TOO_LARGE);
         return IntStream
                .rangeClosed(0, numberString.length() - subLen)
-               .mapToObj(x -> 
-                         numberString.substring(x, x + subLen))
+               .mapToObj(x -> numberString.substring(x, x + subLen))
                .collect(Collectors.toList());
     }
 }
