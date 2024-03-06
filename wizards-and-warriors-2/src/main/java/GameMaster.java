b7c670803fb7c76b4c7e71fe4df6f9511b926f07
Zero Delta <zerodeltahedge@gmail.com>
2024-03-06 23:06:48 +0200

    Update GameMaster.java

--- wizards-and-warriors-2/src/main/java/GameMaster.java
+++ wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,16 +1,25 @@
 public class GameMaster {
     public String describe(Character character) {
-        return String.format("You're a level %d %s with %s hit points.",
-                             character.getLevel(),
-                             character.getCharacterClass(),
-                             character.getHitPoints());
+        return String.format(
+            "You're a level %d %s with %s hit points.",
+            character.getLevel(),
+            character.getCharacterClass(),
+            character.getHitPoints());
     }
 
     public String describe(Destination destination) {
-    
+        return String.format(
+            "You've arrived at %s, which has %d inhabitants.",
+            destination.getName(),
+            destination.getInhabitants());
     }
 
     public String describe(TravelMethod travelMethod) {
+        return String.format("You're traveling to your destination %s.",
+            switch (travelMethod) {
+            case WALKING -> "by walking";
+            case HORSEBACK -> "on horseback";
+        });;
     }
 
     public String describe(Character c, Destination d, TravelMethod tm) {
