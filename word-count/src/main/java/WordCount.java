fb436cde2f0e40adb178b05e25a458be86e32c55
Zero Delta <zerodeltahedge@gmail.com>
2024-03-15 03:37:49 +0200

    Refactor WordCount (* changed spec)

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -8,24 +8,16 @@ import static java.util.stream.Collectors.counting;
 import static java.util.function.Function.identity;
 
 public class WordCount {
-
     public Map<String, Integer> phrase(String sentence) {
-
         if (sentence == null || sentence.isEmpty())
             return Collections.emptyMap();
-        
         return Stream
-              .of(sentence
-                  .replaceAll("[^\\w\\d']+", " ")
-                  .trim()
-                  .toLowerCase()
-                  .split("\\s+"))
-              .map(w -> 
-                   w.replaceAll("^\\p{Punct}+|\\p{Punct}+$", 
-                                ""))
-              .collect(
-                  groupingBy(identity(), 
-                             collectingAndThen(
-                                counting(), Long::intValue)));
+               .of(sentence.replaceAll("^\\'|\\'$|[^\\w\\d']+", " ")
+                           .trim()
+                           .toLowerCase()
+                           .split("\\s+"))
+               .map(w -> w.replaceAll("^\\p{Punct}+|\\p{Punct}+$", ""))
+               .collect(groupingBy(identity(), 
+                                   collectingAndThen(counting(), Long::intValue)));
     }
 }
