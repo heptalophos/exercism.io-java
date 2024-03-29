e215d4d64a5cd62300a7d7ceaab8546fc3e052b1
Zero Delta <zerodeltahedge@gmail.com>
2024-03-29 20:49:27 +0200

    Refactor SqueakyClean (as per changed spec).

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -2,7 +2,7 @@ class SqueakyClean {
     static String clean(String identifier) {
         StringBuilder cleaned = new StringBuilder(identifier.length());
         boolean wordBoundary = false;
-        for (char ch: identifier.toCharArray()) {
+        for (char ch: convertLeetChars(identifier).toCharArray()) {
             if (isNotGreek(ch)) {
                 if (wordBoundary) {
                     cleaned.append(Character.toUpperCase(ch));
@@ -25,5 +25,15 @@ class SqueakyClean {
 
     private static boolean isNotGreek(char ch) {
         return (ch < 'α' || 'ω' < ch);
+    }
+
+    private static String convertLeetChars(String identifier) {
+        return identifier.replaceAll("0", "o")
+                         .replaceAll("1", "l")
+                         .replaceAll("3", "e")
+                         .replaceAll("4", "a")
+                         .replaceAll("5", "s")
+                         .replaceAll("7", "t");
+
     }
 }
