ab06a0f5e7d0d3e92444a37fab8a2dbd9eca76aa
Zero Delta <zerodeltahedge@gmail.com>
2024-04-15 23:31:42 +0300

    Refactor Fighter - (per changed specification)

--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -1,39 +1,28 @@
-abstract class Fighter {
-
-    boolean isVulnerable() {
-        return false;
-    }
-
-    abstract int damagePoints(Fighter fighter);
-
+class Fighter {
+    boolean isVulnerable() { return true; }
+    int getDamagePoints(Fighter fighter) { return 1; }
     public String toString() {
         return "Fighter is a " + getClass().getSimpleName();
     }
 }
 
 class Warrior extends Fighter {
-
     @Override
-    int damagePoints(Fighter wizard) {
+    boolean isVulnerable() { return false; }
+    int getDamagePoints(Fighter wizard) { 
         return wizard.isVulnerable() ? 10 : 6;
     }
+    @Override
+    public String toString() { return super.toString(); }
 }
 
 class Wizard extends Fighter {
-
     private boolean spellReady = false; 
-
     @Override
-    boolean isVulnerable() {
-        return spellReady == false;
-    }
-
+    boolean isVulnerable() { return spellReady == false; }
     @Override
-    int damagePoints(Fighter warrior) {
-        return spellReady ? 12 : 3;
-    }
-
-    void prepareSpell() {
-        spellReady = true;
-    }
+    int getDamagePoints(Fighter warrior) { return spellReady ? 12 : 3; }
+    void prepareSpell() { spellReady = true; }
+    @Override
+    public String toString() { return super.toString(); }
 }
