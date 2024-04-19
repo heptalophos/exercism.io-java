fc30099b9302dac0719d45c681b7186afa0a0bdc
Zero Delta <zerodeltahedge@gmail.com>
2024-04-19 23:03:55 +0300

    Update Knapsack.java

--- knapsack/src/main/java/Knapsack.java
+++ knapsack/src/main/java/Knapsack.java
@@ -19,3 +19,19 @@ public class Knapsack {
                 .orElse(0);
     }
 }
+
+public class Item {
+    private final int value;
+    private final int weight;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
