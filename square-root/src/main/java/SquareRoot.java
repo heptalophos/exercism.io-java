cc98ad23275a98c1b49ff7547a129967cf18f1b4
Zero Delta <zerodeltahedge@gmail.com>
2024-04-30 17:16:12 +0300

    SquareRoot - 1st passing all tests

--- square-root/src/main/java/SquareRoot.java
+++ square-root/src/main/java/SquareRoot.java
@@ -1,5 +1,13 @@
 public class SquareRoot {
     public int squareRoot(int radicand) {
-        throw new UnsupportedOperationException("Please implement the SquareRoot.squareRoot method.");
+        int min = 1;
+        int max = radicand;
+        int mid = min;
+        while ( mid * mid != radicand ) {
+            mid = (max + min) >> 1;
+            if ( mid * mid > radicand ) max = mid;
+            else min = mid;
+        }            
+        return mid;
     }
 }
