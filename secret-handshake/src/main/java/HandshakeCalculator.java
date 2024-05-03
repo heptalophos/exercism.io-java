69ee2607e418993afc3bd87a0454ec49f3fb8d31
Zero Delta <zerodeltahedge@gmail.com>
2024-05-03 19:17:59 +0300

    Reformat HandshakeCalculator.java

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -2,13 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 class HandshakeCalculator {
-
     private final List<Signal> actions = new ArrayList<>();
 
     public List<Signal> calculateHandshake(int number) {
-
         if ((0x01 & number) != 0) 
             actions.add(Signal.WINK);
         if ((0x02 & number) != 0) 
@@ -19,7 +16,6 @@ class HandshakeCalculator {
             actions.add(Signal.JUMP);
         if ((0x10 & number) != 0) 
             Collections.reverse(actions);
-        
         return actions;
     }
 }
