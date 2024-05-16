1f69e463a934bc6ad53bddd1ca1f24de5fb7d41b
Zero Delta <zerodeltahedge@gmail.com>
2024-05-17 01:56:24 +0300

    Refactor Darts.java

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -3,11 +3,12 @@ class Darts {
     Darts() {}
 
     int score(double x, double y) {
+        int points = 0;
         double r = Math.sqrt(x*x + y*y);
-        if (r <= 1)  return 10;
-        if (r <= 5)  return 5;
-        if (r <= 10) return 1;
-        return 0;
+        if (r <= 10) points += 1;
+        if (r <= 5)  points += 4;
+        if (r <= 1)  points += 5;
+        return points;
     }
 
 }
