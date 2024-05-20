5a224fdf210dd23197181556c9fb5b605fb11c1a
Zero Delta <zerodeltahedge@gmail.com>
2024-05-21 01:49:18 +0300

    Reformat Knapsack

--- knapsack/src/main/java/Knapsack.java
+++ knapsack/src/main/java/Knapsack.java
@@ -2,20 +2,17 @@ import java.util.List;
 import static java.util.stream.IntStream.range;
 
 public class Knapsack {
-
     public int maximumValue(final int capacity, final List<Item> items) {
         return range(0, items.size())
-               .filter(x -> 
-                    items.get(x).getWeight() <= capacity
-                )
+               .filter(x -> items.get(x).getWeight() <= capacity)
                .map(x -> { 
-                    Item item = items.get(x);
-                    int value = item.getValue();
-                    int weight = item.getWeight();
-                    List<Item> remainingItems = 
+                        Item item = items.get(x);
+                        int value = item.getValue();
+                        int weight = item.getWeight();
+                        List<Item> remaining = 
                             items.subList(x + 1, items.size());
-                    return  value + 
-                            maximumValue(capacity - weight, remainingItems);
+                        return value + 
+                               maximumValue(capacity - weight, remaining);
                     }
                 )
                 .max().orElse(0);
