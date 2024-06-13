f767aa5acd7a004bbb7e66c3bd48b5327da93b0d
Zero Delta <zerodeltahedge@gmail.com>
2024-06-13 19:14:53 +0300

    Update MicroBlog.java

--- micro-blog/src/main/java/MicroBlog.java
+++ micro-blog/src/main/java/MicroBlog.java
@@ -1,5 +1,9 @@
 class MicroBlog {
-    public String truncate(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private static final int MAX_POST = 5;
+
+    public String truncate(final String input) {
+        return input.codePointCount(0, input.length()) >= MAX_POST
+               ? input.substring(0, input.offsetByCodePoints(0, MAX_POST))
+               : input;
     }
 }
