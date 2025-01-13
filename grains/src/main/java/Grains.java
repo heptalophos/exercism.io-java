f5d29a8b60904287dff92f827adf8a61180edbb5
Zero Delta <zerodeltahedge@gmail.com>
2025-01-14 01:06:13 +0200

    Reformat Grains.java

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -1,26 +1,16 @@
 import java.math.BigInteger;
 
 class Grains {
-
-    private static final String error = 
-            "square must be between 1 and 64";
+    private static final String error = "square must be between 1 and 64";
 
     BigInteger grainsOnSquare(final int square) {
-
         if (square < 1 || square > 64) {
-            throw 
-                new IllegalArgumentException(error);
+            throw new IllegalArgumentException(error);
         }
-
-        return BigInteger.ONE
-                         .shiftLeft(square - 1);
+        return BigInteger.ONE.shiftLeft(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-
-        return BigInteger.ONE
-                         .shiftLeft(64)
-                         .subtract(BigInteger.ONE);
+        return BigInteger.ONE.shiftLeft(64).subtract(BigInteger.ONE);
     }
-
 }
