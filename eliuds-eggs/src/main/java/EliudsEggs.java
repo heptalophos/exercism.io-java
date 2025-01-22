efcacd36019b9d07d0d0a27c8552a718ec4c3dba
Zero Delta <zerodeltahedge@gmail.com>
2025-01-22 03:06:27 +0200

    Refactor EliudsEggs.java

--- eliuds-eggs/src/main/java/EliudsEggs.java
+++ eliuds-eggs/src/main/java/EliudsEggs.java
@@ -1,6 +1,10 @@
 public class EliudsEggs {
     public int eggCount(int number) {
-        if (number == 0) return 0;
-        return number % 2 + eggCount(number / 2);
+        int eggs = 0;
+        while (number > 0) {
+            if (~(~number | ~1) != 0) eggs += 1;
+            number >>= 1;
+        }
+        return eggs;
     }
 }
